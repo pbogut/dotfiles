ca161885829a15d1057919aceed12f37cd3c7f3f
Pawel Bogut <pbogut@smeagol.pl>
2016-02-11 11:54:18 +0000

    Add TextAid chroe extention support

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,3 +1,13 @@
 #super awesome capslock key
 setxkbmap -option 'caps:ctrl_modifier'
 xcape -e 'Caps_Lock=Escape'
+#chrome textaid extention server
+script=$(readlink -f "$0")
+scriptpath=$(dirname "$script")
+pidfile=/tmp/textaid.pid
+if [ -f "$pidfile" ] && kill -0 `cat $pidfile` 2>/dev/null; then
+    echo 'ekhm....' > /dev/null
+else
+    perl "$scriptpath/edit-server.pl" &
+    echo $! > $pidfile
+fi
