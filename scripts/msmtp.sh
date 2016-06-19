c5814e13f88c6453cb91b27b12ff3f40ea8e7d21
Pawel Bogut <pbogut@smeagol.pl>
2016-06-19 22:17:37 +0100

    typo

--- scripts/msmtp.sh
+++ scripts/msmtp.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cat /dev/stdin | msmtp -a $1 --passwordeval="get-config.sh gemail-$1-passwd" --user=`get-config.sh email-$1-user` --from=`get-config.sh email-$1-from` ${@:2}
+cat /dev/stdin | msmtp -a $1 --passwordeval="get-config.sh email-$1-passwd" --user=`get-config.sh email-$1-user` --from=`get-config.sh email-$1-from` ${@:2}
