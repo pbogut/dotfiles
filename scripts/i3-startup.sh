284de4c7853408187e2682712818f0c3f7bcfc4e
Pawel Bogut <pbogut@smeagol.pl>
2016-07-21 15:51:43 +0100

    i3 config

--- scripts/i3-startup.sh
+++ scripts/i3-startup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+xrandr --output HDMI-1 --mode 1920x1080 --pos 1920x0 --output HDMI-2 --primary --mode 1920x1080 --pos 0x0
 xrandr --output eDP1 --mode 1368x768 --pos 1920x0 --output HDMI1 --primary --mode 1920x1080 --pos 0x0
+feh --randomize --bg-fill  ~/Pictures/wallpaper
 source ~/.scripts/autostart.sh
