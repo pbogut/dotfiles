201f0b3dba806e673db823a6fe71874a57222603
Pawel Bogut <pbogut@smeagol.pl>
2016-07-21 23:24:04 +0100

    i3 config

--- scripts/i3-startup.sh
+++ scripts/i3-startup.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-xrandr --output HDMI-1 --mode 1920x1080 --pos 1920x0 --output HDMI-2 --primary --mode 1920x1080 --pos 0x0
-xrandr --output eDP1 --mode 1368x768 --pos 1920x0 --output HDMI1 --primary --mode 1920x1080 --pos 0x0
-feh --randomize --bg-fill  ~/Pictures/wallpaper
+#xrandr --output HDMI-1 --mode 1920x1080 --pos 1920x0 --output HDMI-2 --primary --mode 1920x1080 --pos 0x0
+#xrandr --output eDP1 --mode 1368x768 --pos 1920x0 --output HDMI1 --primary --mode 1920x1080 --pos 0x0
+hostname=`hostname`
+set_walpaper () {
+    feh --randomize --bg-fill  ~/Pictures/wallpaper
+}
+if [ "$hostname" == "v3engine" ]; then # my laptop
+    (
+        sleep 3
+        xrandr --output HDMI1 --auto --left-of eDP1
+        xrandr --output eDP1 --mode 1368x768 --pos 1920x0
+        set_walpaper
+    )  &
+elif [ "$hostname" == "darkbox" ]; then # pc at work
+    xrandr --output HDMI-1 --mode 1920x1080 --pos 1920x0 --output HDMI-2 --primary --mode 1920x1080 --pos 0x0
+    set_walpaper
+else
+    set_walpaper
+fi
 source ~/.scripts/autostart.sh
