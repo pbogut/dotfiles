f00bd23cd402d0daec273603d9029b2aba05d10d
Pawel Bogut <pbogut@smeagol.pl>
2016-08-10 20:36:01 +0100

    (c) Microsoft 1988

---
+++ scripts/mpd-spotify-add-track.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+track=`mpc current -f '%title% - %artist% - %album%'`
+query=`echo "$track" | sed 's/ /%20/g'`
+track_id=`curl -s -X GET "https://api.spotify.com/v1/search?q=$query&type=track" | grep '"uri" : "spotify:track:' | sed 's/.*:track:\(.*\)"/\1/g' | head -n 1`
+token=`cat ~/.spotify-api-token`
+if [ -z $token ]; then
+    echo "Token not found, you need to place token in ~/.spotify-api-token file"
+    exit 1
+fi
+if [ $track_id == "" ]; then
+    echo "Can't find the track:"
+    echo -e "\t$track (id:$track_id)"
+    exit 1
+fi
+
+echo "Adding track to library:"
+echo -e "\t$track (id:$track_id)"
+curl -s -X PUT "https://api.spotify.com/v1/me/tracks?ids=$track_id" -H "Accept: application/json" -H "Authorization: Bearer $token"
+
+
