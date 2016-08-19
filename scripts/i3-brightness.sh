789f9c9238684e64037643f6593496fd4c9b8143
Pawel Bogut <pbogut@smeagol.pl>
2016-08-19 22:28:29 +0100

    Add feedback

--- scripts/i3-brightness.sh
+++ scripts/i3-brightness.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-current_brightness=`cat /sys/class/backlight/intel_backlight/brightness`
+brightness=`cat /sys/class/backlight/intel_backlight/brightness`
 max_brightness=`cat /sys/class/backlight/intel_backlight/max_brightness`
-if [ "$1" == "up" ] && (($current_brightness < $max_brightness)); then
-    brightness=`expr $current_brightness + 100`
+if [ "$1" == "up" ] && (($brightness < $max_brightness)); then
+    brightness=`expr $brightness + 100`
     echo "echo $brightness > /sys/class/backlight/intel_backlight/brightness" | sudo bash
 fi
-if [ "$1" == "down" ] && (($current_brightness > 0)); then
-    brightness=`expr $current_brightness - 100`
+if [ "$1" == "down" ] && (($brightness > 0)); then
+    brightness=`expr $brightness - 100`
     echo "echo $brightness > /sys/class/backlight/intel_backlight/brightness" | sudo bash
 fi
+echo "Brightness: `expr 100 \* $brightness / $max_brightness`%"
