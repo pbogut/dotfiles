045abdf298fc2e3ba58d271f9e4503ad3007094c
Pawel Bogut <pbogut@smeagol.pl>
2016-09-17 15:46:57 +0100

    Fixing David's bugs.

--- scripts/i3-dmenu-run-in-terminal.sh
+++ scripts/i3-dmenu-run-in-terminal.sh
@@ -2,6 +2,7 @@
 commands=`compgen -c | tail -n+26`
 zsh_aliases=`zsh -ic alias | sed 's/^\([^=]*\)=.*/\1/g'`
 echo "$commands\n$zsh_aliases" | sort | uniq | dmenu -l 20 -p term | while read cmd; do
+    # roxterm --separate -e zsh -ic "$cmd"
     roxterm -e zsh -ic "$cmd"
 done
 
