b974610479c54f87086e4e9a229c2ec63d6bc992
Pawel Bogut <pbogut@smeagol.pl>
2016-10-03 20:29:47 +0100

    Mail to web script improve

--- scripts/mail-to-web.sh
+++ scripts/mail-to-web.sh
@@ -5,5 +5,5 @@ else
     email_content="$1"
 fi
 
-query=`cat "$email_content" | grep '^Message-ID: ' | sed 's#^Message-ID: <\(.*\)>#\1#g' | ruby -ne 'require "cgi"; puts CGI.escape($_)'`
+query=`cat "$email_content" | sed '/^Message-ID:/,$!d' | head -n 2 | sed 's/^Message-ID:[^<]*//g' | grep -v '^$' | sed 's#[^<]*<\(.*\)>.*#\1#g' | ruby -ne 'require "cgi"; puts CGI.escape($_)'`
 chromium  "http://localhost:6245?q=$query"
