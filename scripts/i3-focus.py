0295778e07583090dd453d8b21b68887b94cd735
Pawel Bogut <pbogut@smeagol.pl>
2016-10-19 22:21:30 +0100

    assorted changes

--- scripts/i3-focus.py
+++ scripts/i3-focus.py
@@ -2,27 +2,31 @@
 import re
 import i3
 import subprocess
-import sys
 import argparse
+
 
 def validate_direction(parser, val):
     if val not in ['left', 'right', 'up', 'down']:
         parser.error('{} is not a valid direction.'.format(val))
     return val
 
-parser = argparse.ArgumentParser(description='Toggle between visible workspaces.')
+parser = argparse.ArgumentParser(
+    description='Toggle between visible workspaces.')
 parser.add_argument('direction',
                     help='Focus direction, can be left, right, up or down',
                     type=lambda val: validate_direction(parser, val))
 parser.add_argument('--skip-vim', dest='skip_vim', action='store_const',
-                    help='swich to the provious workspace (by default it goes to next)',
+                    help='swich to the provious workspace' +
+                    ' (by default it goes to next)',
                     const=True, default=False)
 
 
 args = parser.parse_args()
 
+
 def is_vim_in_tmux(session_id):
-    if args.skip_vim: return False
+    if args.skip_vim:
+        return False
 
     grep = " | grep '1$' |sed 's/1$//'"
     command = "tmux list-pane -F '#{pane_current_command}#{pane_active}'" + grep
@@ -50,21 +54,28 @@ def is_tmux_edge(session_id, direction):
         'height': list_command + "-F '#{window_height}#{pane_active}'" + grep
     }
 
-    proc = subprocess.Popen(commands[direction], stdout=subprocess.PIPE, shell=True)
+    proc = subprocess.Popen(
+        commands[direction], stdout=subprocess.PIPE, shell=True)
     offset = int(proc.stdout.read())
 
     if direction == "right":
-        proc = subprocess.Popen(commands["width"], stdout=subprocess.PIPE, shell=True)
+        proc = subprocess.Popen(
+            commands["width"], stdout=subprocess.PIPE, shell=True)
         window_width = int(proc.stdout.read())
     if direction == "down":
-        proc = subprocess.Popen(commands["height"], stdout=subprocess.PIPE, shell=True)
+        proc = subprocess.Popen(
+            commands["height"], stdout=subprocess.PIPE, shell=True)
         window_height = int(proc.stdout.read())
-        print (window_height)
+        print(window_height)
 
-    if direction == "left" and offset == 0: return True
-    if direction == "right" and window_width - offset == 1: return True
-    if direction == "up" and offset == 0: return True
-    if direction == "down" and window_height - offset == 1: return True
+    if direction == "left" and offset == 0:
+        return True
+    if direction == "right" and window_width - offset == 1:
+        return True
+    if direction == "up" and offset == 0:
+        return True
+    if direction == "down" and window_height - offset == 1:
+        return True
 
     return False
 
@@ -87,19 +98,31 @@ if __name__ == '__main__':
     current = i3.filter(nodes=[], focused=True)[0]
     name = current['name']
 
-    if  bool(re.match(r'.* tmux \$\d+:\d+:\d+$', name)):
+    if bool(re.match(r'.* tmux \$\d+:\d+:\d+$', name)):
         session_id = "$" + re.search(r"tmux \$(\d+):", name).group(1)
 
         if is_vim_in_tmux(session_id):
-            # send esc to make sure your in normal mode (its buggy sometimes ;S)
-            subprocess.Popen("tmux send-keys -t \\" + session_id + " Escape", shell=True)
+            # send esc to make sure your in normal mode (its buggy sometimes
+            # ;S)
+            subprocess.Popen("tmux send-keys -t \\" +
+                             session_id + " Escape", shell=True)
             # then send I3Focus trigger combination
-            subprocess.Popen("tmux send-keys -t \\" + session_id + " " + vim_dir[direction], shell=True)
+            subprocess.Popen("tmux send-keys -t \\" +
+                             session_id + " " + vim_dir[direction], shell=True)
         else:
 
             if is_tmux_edge(session_id, direction):
                 i3.focus(direction)
             else:
-                subprocess.Popen("tmux select-pane -t \\" + session_id + " " + pane_dir[direction], shell=True)
+                subprocess.Popen("tmux select-pane -t \\" +
+                                 session_id + " " +
+                                 pane_dir[direction], shell=True)
     else:
-        i3.focus(direction)
+        if bool(re.match(r'^nvim:.*$', name)) and not args.skip_vim:
+            vim_id = "/tmp/nvim" + \
+                re.search(r"^nvim:(.*)$", name).group(1) + "/0"
+            subprocess.Popen("nvr --servername " + vim_id +
+                             " --remote-send '<esc><" + vim_dir[direction] +
+                             ">'", shell=True)
+        else:
+            i3.focus(direction)
