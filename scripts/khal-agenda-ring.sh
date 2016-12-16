221157d861ba58cab416855153734722f844da9f
Pawel Bogut <pbogut@smeagol.pl>
2016-12-16 20:16:06 +0000

    Fix morning hours issue

--- scripts/khal-agenda-ring.sh
+++ scripts/khal-agenda-ring.sh
@@ -6,13 +6,14 @@ while :; do
         endtime=$(echo $l | sed 's/[^-]*-\([0-9]*\):\([0-9]*\).*/\1\2/')
         curtime=$(date '+%H%M')
         # display event only if not fineshed yet
-        if [[ "$endtime" -gt "$curtime" ]]; then
+        if [[ ${endtime#0} -gt ${curtime#0} ]]; then
             has_event=true
             echo '{"full_text": "'`echo $l | sed 's/"/'"'"'/g'`'"}'
             sleep 15s
         fi
     done
     if [[ -z $has_event ]]; then
+        echo '{"full_text": ""}'
         sleep 60s #no events so wait a bit longer befor calling khal again
     fi)
 done;
