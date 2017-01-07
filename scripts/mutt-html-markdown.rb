5c172e2ee457d4e4de57f597e35a38ed2f4121af
Pawel Bogut <pbogut@smeagol.pl>
2017-01-07 14:24:44 +0000

    Send html emails with mutt

---
+++ scripts/mutt-html-markdown.rb
@@ -0,0 +1,52 @@
+#!/bin/ruby
+#=================================================
+# name:   mutt-html-markdown.rb
+# author: Pawel Bogut <http://pbogut.me>
+# date:   06/01/2017
+#=================================================
+srcfile = File.open(ARGV[0], 'rb')
+mail = srcfile.read
+
+body = mail.gsub(/\A.*?^$\n/m, '')
+body.gsub!(/^(.*)$/,'\1  ') # force line break everywhere
+
+new_body =  ''
+pre_block = false
+body.split("\n").each do |line|
+  if line.match(/^>.*$/) && !pre_block
+    new_body << "<pre class=\"quoted\">\n"
+    pre_block = true
+  end
+
+  if !line.match(/^>.*$/) && pre_block
+    new_body << "</pre>\n"
+    pre_block = false
+  end
+
+  new_body << line + "\n"
+end
+# if body ended on quoted text, then close pre
+new_body << "</pre>\n" if pre_block
+
+html = `markdown <<< "#{new_body}"`
+
+result = <<HTML
+<html>
+<head>
+<meta name="generator" content="mutt-html-markdown/0.1">
+<style>
+code { font-family: 'Andale Mono', 'Lucida Console', 'Bitstream Vera Sans Mono', 'Courier New', monospace; }
+pre { border-left: 20px solid #ddd; margin-left: 10px; padding-left: 5px; }
+pre.quoted { border-left: 5px solid #ddd; margin-left: 0; }
+</style>
+</head>
+<body>
+#{html}
+</body>
+</html>
+HTML
+
+File.open(File.expand_path('~/.mutt/html-markdown-alternative.html'), 'w+') do |file|
+  file.write(result)
+end
+puts result
