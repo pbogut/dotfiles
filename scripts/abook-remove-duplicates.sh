5bc140cfe16bb05b9734b661bec200359bdfff31
Pawel Bogut <pbogut@smeagol.pl>
2017-01-07 21:46:09 +0000

    Better duplicates removing

--- scripts/abook-remove-duplicates.sh
+++ scripts/abook-remove-duplicates.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 file=~/.abook/addressbook
 tmp_file=/tmp/addressbook.tmp
-# remove duplicates
-abook --convert --infile $file --outformat csv |
-        sort | uniq |
-        grep -v "newsletter@\|norepl\|no-repl\|@docs.google.com\|@bugs.bugherd.com\|(via Google [^)]*)" |
-        grep  "@" |
-        abook --convert --informat csv --outformat abook > $tmp_file
-mv "$tmp_file" "$file"
-# dont want nicks
-cat "$file" | sed '/^nick=.*/d' > "$tmp_file"
-mv "$tmp_file" "$file"
+res_file=/tmp/addressbook.res
 
+if [[ ! -e $tmp_file ]]; then
+    cp "$file" "$tmp_file"
+    # remove duplicates
+    abook --convert --infile $tmp_file --outformat csv |
+            ruby -ne '
+                require "csv"
+                row = CSV.parse($_)[0]
+                row[1].downcase!
+                puts row.to_csv' |
+            sort | uniq |
+            grep -v "newsletter@\|norepl\|no-repl\|@docs.google.com\|@bugs.bugherd.com\|(via Google [^)]*)" |
+            grep  "@" |
+            abook --convert --informat csv --outformat abook > $res_file
+    mv "$res_file" "$file"
+    rm "$tmp_file"
+fi
