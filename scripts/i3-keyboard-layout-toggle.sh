b9faf691ee50cd4f6fd4d9af58d9a5168d4c4ecd
Pawel Bogut <pawel.bogut@gmail.com>
2017-10-08 09:37:21 +0200

    Make that it works in 90% of the cases.  3:30.

--- scripts/i3-keyboard-layout-toggle.sh
+++ scripts/i3-keyboard-layout-toggle.sh
@@ -20,14 +20,20 @@ eval $(setxkbmap -query | sed 's#\<\([^\>]*\):.*\<\(.*\)\>#\1="\2"#' | grep -v o
 # echo $model
 # echo $variant
 
-if [[ $variant == "colemak" ]] && [[ $layout == "pl" ]]; then
-    setxkbmap gb
-elif [[ $variant == "" ]] && [[ $layout == "pl" ]]; then
-    setxkbmap pl -variant colemak
-elif [[ $variant == "" ]] && [[ $layout == "gb" ]]; then
-    setxkbmap gb -variant colemak
-elif [[ $variant == "colemak" ]] && [[ $layout == "gb" ]]; then
+if [[ $variant == "colemak" ]]; then
     setxkbmap pl
 else
-    setxkbmap pl
+    setxkbmap pl -variant colemak
 fi
+
+# if [[ $variant == "colemak" ]] && [[ $layout == "pl" ]]; then
+#     setxkbmap gb
+# elif [[ $variant == "" ]] && [[ $layout == "pl" ]]; then
+#     setxkbmap pl -variant colemak
+# elif [[ $variant == "" ]] && [[ $layout == "gb" ]]; then
+#     setxkbmap gb -variant colemak
+# elif [[ $variant == "colemak" ]] && [[ $layout == "gb" ]]; then
+#     setxkbmap pl
+# else
+#     setxkbmap pl
+# fi
