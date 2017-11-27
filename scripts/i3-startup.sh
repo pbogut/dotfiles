ce84ac894d92c76d6a5bc4370fcbaf30ee85c5a6
Pawel Bogut <pawel.bogut@gmail.com>
2017-11-27 08:39:30 +0100

    Fucking templates.

--- scripts/i3-startup.sh
+++ scripts/i3-startup.sh
@@ -11,10 +11,10 @@ if [ "$hostname" == "v3engine" ]; then # my laptop
         xrandr --newmode "1408x790"  91.09  1408 1480 1632 1856  790 791 794 818  -HSync +Vsync
         xrandr --addmode eDP-1 "1408x790"
         if [[ $? == 0 ]]; then
-          xrandr --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --output eDP-1 --mode 1408x790 --pos 1920x290
+          xrandr --output HDMI-1 --primary --mode 1920x1080 --pos 1408x0 --output eDP-1 --mode 1408x790 --pos 0x290
         else
           xrandr --addmode eDP1 "1408x790"
-          [[ $? == 0 ]] && xrandr --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --output eDP1 --mode 1408x790 --pos 1920x290
+          [[ $? == 0 ]] && xrandr --output HDMI1 --primary --mode 1920x1080 --pos 1408x790x0 --output eDP1 --mode 1408x790 --pos 0x290
         fi
         i3-msg restart
         set_walpaper
