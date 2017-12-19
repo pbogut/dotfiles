854afd70606a4237271fa64c9715f2e08a120d12
Pawel Bogut <pawel.bogut@gmail.com>
2017-12-19 23:54:26 +0100

    I can't believe it took so long to fix this.

--- install-bin.sh
+++ install-bin.sh
@@ -7,24 +7,25 @@
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # dotfiles directory
 cd bin
 get() {
-  echo -n "Downloading $2 from $1 ..."
+  echo -n "Downloading $1 from $2 ..."
   if hash curl 2>&- ; then
-      curl -s -L "$1" -o "$2"
+      curl -s -L "$2" -o "$1"
   elif hash wget 2>&- ; then
-      wget -q --no-check-certificate -O "$1" "$2"
+      wget -q --no-check-certificate -O "$2" "$1"
   else
      echo "error! You need to have curl or wget installed."
      exit 1
    fi
-   chmod +x $2
+   chmod +x $1
    echo "done"
 }
 
-get 'https://raw.githubusercontent.com/colinmollenhour/modman/master/modman' modman
-get 'https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar' phpcs
-get 'https://squizlabs.github.io/PHP_CodeSniffer/phpcbf.phar' phpcbf
-get 'http://static.phpmd.org/php/latest/phpmd.phar' phpmd
-get 'http://getcomposer.org/composer.phar' composer
-get 'https://files.magerun.net/n98-magerun.phar' n98-magerun
+get composer 'http://getcomposer.org/composer.phar'
+get modman 'https://raw.githubusercontent.com/colinmollenhour/modman/master/modman'
+get n98-magerun 'https://files.magerun.net/n98-magerun.phar'
+get phpcbf 'https://squizlabs.github.io/PHP_CodeSniffer/phpcbf.phar'
+get phpcs 'https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar'
+get phpmd 'http://static.phpmd.org/php/latest/phpmd.phar'
+get robo 'http://robo.li/robo.phar'
 
 cd ..
