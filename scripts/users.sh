b0bfe417aeb22793225acf5fc6b5e28187e61ccf
Pawel Bogut <pbogut@pbogut.me>
2018-06-15 17:46:50 +0200

    more fixes

--- scripts/users.sh
+++ scripts/users.sh
@@ -14,8 +14,10 @@ if [[ $1 == '--register' ]]; then
     echo "$user:$XDG_VTNR" >>$tmp
 fi
 if [[ $1 == '--switch' ]]; then
-    users=$(cat /etc/passwd | grep ':x:[0-9]*:100:' | sed 's,\([^:]*\).*,\1,')
+    # users=$(cat /etc/passwd | grep ':x:[0-9]*:100:' | sed 's,\([^:]*\).*,\1,')
+    users=$(dm-tool list-seats | grep UserName | sed 's/.*UserName=.\(.*\).$/\1/' | grep -v $(id -un))
     user=$(echo "$users" | rofi -dmenu)
+    [[ -z $user ]] && exit 0
     islogged=$(dm-tool list-seats 2>/dev/null | grep "UserName='$user'")
     if [[ -z $islogged ]]; then
         # not loggod
