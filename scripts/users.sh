b419a8675c26e7ec5063ea6936280e0ac8cce2d1
Pawel Bogut <pbogut@pbogut.me>
2018-06-15 17:19:44 +0200

    Something fixed

---
+++ scripts/users.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#=================================================
+# name:   switch-user.sh
+# author: Pawel Bogut <https://pbogut.me>
+# date:   15/06/2018
+#=================================================
+tmp="/tmp/users"
+if [[ $1 == '--register' ]]; then
+    user=$(id -un)
+    touch $tmp
+    chmod 777 $tmp
+    sed -i "/^$user:/d" $tmp
+    echo "$user:$XDG_VTNR" >>$tmp
+fi
+if [[ $1 == '--switch' ]]; then
+    users=$(cat /etc/passwd | grep ':x:[0-9]*:100:' | sed 's,\([^:]*\).*,\1,')
+    user=$(echo "$users" | rofi -dmenu)
+    islogged=$(dm-tool list-seats 2>/dev/null | grep "UserName='$user'")
+    if [[ -z $islogged ]]; then
+        # not loggod
+        dm-tool switch-to-user $user
+    else
+        # logged
+        vtn=$(cat $tmp | grep "^$user" | sed 's,[^:]*:,,')
+    fi
+    if [[ -n $vtn ]]; then
+        sudo chvt $vtn
+        dm-tool switch-to-user $user
+    fi
+fi
