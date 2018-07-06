ce440342dc9a241b86767bfb28aaa67644559b16
Pawel Bogut <pbogut@pbogut.me>
2018-07-06 16:33:11 +0200

    arrgghh... damn this thing for not working.

--- install.sh
+++ install.sh
@@ -6,11 +6,11 @@
 
 ########## Variables
 
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # dotfiles directory
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" # dotfiles directory
 echo -n "Updating submodules ..."
-git submodule update --init
+git submodule sync && git submodule update --init
 echo "done"
-olddir=~/.dotfiles_backup/`date +%s%N`    # old dotfiles backup directory
+olddir=~/.dotfiles_backup/$(date +%s%N) # old dotfiles backup directory
 read -d '' files <<"EOF"
     ackrc
     bin
@@ -94,64 +94,64 @@ echo "done"
 # create required directories
 echo "Creating missing directories"
 for directory in $directories; do
-    if  [ ! -e ~/$directory ]; then
-        echo -en "\t~/$directory "
-        mkdir -p ~/$directory
-        echo "done"
-    fi
+  if [ ! -e ~/$directory ]; then
+    echo -en "\t~/$directory "
+    mkdir -p ~/$directory
+    echo "done"
+  fi
 done
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 echo "Backing up current files to $olddir/"
 for file in $files; do
-    if [ -e ~/.$file ]; then
-        echo -en "\t~/.$file "
-        mv ~/.$file $olddir/
-        echo "done"
-    fi
+  if [ -e ~/.$file ]; then
+    echo -en "\t~/.$file "
+    mv ~/.$file $olddir/
+    echo "done"
+  fi
 done
 echo "Creating symlinks in home directory"
 for file in $files; do
-    echo -en "\t~/.$file "
-    # crate directory if not exists
-    mkdir -p `dirname $HOME/.$file`
-    ln -s $dir/$file $HOME/.$file
-    echo "done"
+  echo -en "\t~/.$file "
+  # crate directory if not exists
+  mkdir -p $(dirname $HOME/.$file)
+  ln -s $dir/$file $HOME/.$file
+  echo "done"
 done
 
 echo "Post install actions"
 
 echo -en "\tMaking scripts executable ... "
-find $dir -name "*.sh" -exec chmod +x {} \; > /dev/null 2>&1
-find $dir -name "*.zsh" -exec chmod +x {} \; > /dev/null 2>&1
-find $dir -name "*.phar" -exec chmod +x {} \; > /dev/null 2>&1
-chmod +x ~/.scripts/* > /dev/null 2>&1
-chmod +x ~/.offlineimap-hooks/* > /dev/null 2>&1
+find $dir -name "*.sh" -exec chmod +x {} \; >/dev/null 2>&1
+find $dir -name "*.zsh" -exec chmod +x {} \; >/dev/null 2>&1
+find $dir -name "*.phar" -exec chmod +x {} \; >/dev/null 2>&1
+chmod +x ~/.scripts/* >/dev/null 2>&1
+chmod +x ~/.offlineimap-hooks/* >/dev/null 2>&1
 echo "done"
 
 echo -en "\tSet ranger as default manager (xdg-mime) ... "
-[[ -n `command -v xdg-mime` ]] && xdg-mime default ranger.desktop inode/directory
+[[ -n $(command -v xdg-mime) ]] && xdg-mime default ranger.desktop inode/directory
 echo "done"
 
 echo -en "\tSet browser script as default browser (xdg-settings) ... "
-[[ -n `command -v xdg-mime` ]] && xdg-settings set default-web-browser browser.desktop
+[[ -n $(command -v xdg-mime) ]] && xdg-settings set default-web-browser browser.desktop
 echo "done"
 
 echo -en "\t"$(
-  if [[ -n `command -v gio` ]]; then
+  if [[ -n $(command -v gio) ]]; then
     gio mime inode/directory ranger.desktop
-  elif [[ -n `command -v gvfs-mime` ]]; then
+  elif [[ -n $(command -v gvfs-mime) ]]; then
     gvfs-mime --set inode/directory ranger.desktop
   fi
 ) && echo " ... done"
 
 echo -en "\tInstall vim/neovim plugins ... "
-[[ -n `command -v /bin/vim` ]] && /bin/vim -u ./vim/silent.vimrc +PlugInstall +qa
-[[ -n `command -v /bin/nvim` ]] && /bin/nvim -u ./vim/silent.vimrc +PlugInstall +qa
+[[ -n $(command -v /bin/vim) ]] && /bin/vim -u ./vim/silent.vimrc +PlugInstall +qa
+[[ -n $(command -v /bin/nvim) ]] && /bin/nvim -u ./vim/silent.vimrc +PlugInstall +qa
 echo "done"
 
 echo -e "\nChange your shell if you wish:\n"
-which fish > /dev/null && echo -e "\tchsh $(id -un) -s $(which fish)"
-which zsh > /dev/null && echo -e "\tchsh $(id -un) -s $(which zsh)"
+which fish >/dev/null && echo -e "\tchsh $(id -un) -s $(which fish)"
+which zsh >/dev/null && echo -e "\tchsh $(id -un) -s $(which zsh)"
 
 read -p "Do you want to update bin folder? [yN]" -n 1 -s ask
 echo ""
