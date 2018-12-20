865020313ed968fef49d8d7fa4e84f166520ae7d
Pawel Bogut <pbogut@pbogut.me>
2018-12-20 15:34:15 +0100

    Make msmtp.sh script relay on From field only to select email account

--- scripts/msmtp.sh
+++ scripts/msmtp.sh
@@ -1,2 +1,21 @@
 #!/bin/sh
-cat /dev/stdin | mutt-html-mime.rb | msmtp -a $1 --passwordeval="~/.scripts/get-config.sh email-$1-passwd" --user=`~/.scripts/get-config.sh email-$1-user` --from=`~/.scripts/get-config.sh email-$1-from` ${@:2}
+tmp_file=$(mktemp)
+cat /dev/stdin > $tmp_file
+set -e
+set -o pipefail
+
+from=$(cat $tmp_file |
+    grep '^From: ' | head -n1 |
+    sed 's,From: [^<]*<\(.*\)>,\1,' |
+    sed 's,From: \([^\s]*\),\1,')
+acc=$(echo $from | sed 's,.*@,,')
+
+cat $tmp_file |
+    mutt-html-mime.rb |
+    msmtp -a $acc --passwordeval="~/.scripts/get-config.sh email-$from-passwd" --user=`gpg-config get email-$from-user` --from=$from ${@:1}
+
+
+echo $tmp_file > /tmp/__email_file
+echo $from >> /tmp/__email_var
+echo $acc >> /tmp/__email_var
+echo msmtp -a $acc --passwordeval="~/.scripts/get-config.sh email-$from-passwd" --user=`gpg-config get email-$from-user` --from=$from ${@:1} >> /tmp/__email_var
