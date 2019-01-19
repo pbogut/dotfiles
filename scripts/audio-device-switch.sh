bb9273bf15f2fd60e3fe8410b9bc40bec29cbadd
Pawel Bogut <pbogut@pbogut.me>
2019-01-19 14:25:38 +0100

    Fix when not all indexes are enabled

--- scripts/audio-device-switch.sh
+++ scripts/audio-device-switch.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
+first_sink_index=$(pacmd list-sinks |
+  grep 'index:[[:space:]][[:digit:]]' |
+  sed 's/.*index:[[:space:]]\([[:digit:]]\)/\1/' |
+  head -n1)
+next_sink_index=$(pacmd list-sinks |
+  grep 'index:[[:space:]][[:digit:]]' |
+  sed '/\*[[:space:]]index:[[:space:]][[:digit:]]/I,$!d' |
+  sed 's/.*index:[[:space:]]\([[:digit:]]\)/\1/' |
+  tail -n+2)
 
-declare -i sinks_count=`pacmd list-sinks | grep -c index:[[:space:]][[:digit:]]`
-declare -i active_sink_index=`pacmd list-sinks | sed -n -e 's/\*[[:space:]]index:[[:space:]]\([[:digit:]]\)/\1/p'`
-declare -i major_sink_index=$sinks_count-1
-declare -i next_sink_index=0
-
-if [ $active_sink_index -ne $major_sink_index ] ; then
-	next_sink_index=active_sink_index+1
+if [[ "$next_sink_index" == "" ]]; then
+  next_sink_index=$first_sink_index
 fi
 
 #change the default sink
 pacmd "set-default-sink ${next_sink_index}"
 
 #move all inputs to the new sink
-for app in $(pacmd list-sink-inputs | sed -n -e 's/index:[[:space:]]\([[:digit:]]\)/\1/p');
-do
-	pacmd "move-sink-input $app $next_sink_index"
+for app in $(pacmd list-sink-inputs | sed -n -e 's/index:[[:space:]]\([[:digit:]]\)/\1/p'); do
+  pacmd "move-sink-input $app $next_sink_index"
 done
 
 #display notification
-declare -i ndx=0
-pacmd list-sinks | sed -n -e 's/device.description[[:space:]]=[[:space:]]"\(.*\)"/\1/p' | while read line;
-do
-	if [ $next_sink_index -eq $ndx ] ; then
-		#notify-send -i notification-audio-volume-high "Sound output switched to" "$line"
-		exit
-	fi
-	ndx+=1
-done;
+pacmd list-sinks |
+  sed '/\*[[:space:]]index:[[:space:]][[:digit:]]/I,$!d' |
+  sed -n -e 's/device.description[[:space:]]=[[:space:]]"\(.*\)"/\1/p' |
+  while read line; do
+    notify-send -i audio-volume-high "Sound output switched to" "$line"
+    exit
+  done
