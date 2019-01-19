db4ffe506a54ea4b6b5819c11b1865e7722e5b39
Pawel Bogut <pbogut@pbogut.me>
2019-01-19 15:05:30 +0100

    Get location automatically from ip-api.com

--- scripts/i3-weather.sh
+++ scripts/i3-weather.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
-#AccuWeather (r) RSS weather tool for conky
-#
-#USAGE: weather.sh UKXX0062
-#
-#(c) Michael Seiler 2007
+#AccuWeather (r) RSS weather tool for i3blocks
 
-METRIC=1 #Should be 0 or 1; 0 for F, 1 for C
+metric=1 #Should be 0 or 1; 0 for F, 1 for C
+location=$1
 
 if [ -z $1 ]; then
-echo
-echo "USAGE: weather.sh <locationcode>"
-echo
-exit 0;
+    location=$(curl -s 'http://ip-api.com/json' | jq -r '(.lat|tostring) + "," + (.lon|tostring)')
 fi
-
-curl -s http://rss.accuweather.com/rss/liveweather_rss.asp\?metric\=${METRIC}\&locCode\=$1 | perl -ne 'use utf8; if (/Currently/) {chomp;/\<title\>Currently: (.*)?\<\/title\>/; my @values=split(":",$1); if( $values[0] eq "Sunny" || $values[0] eq "Mostly Sunny" || $values[0] eq "Partly Sunny" || $values[0] eq "Intermittent Clouds" || $values[0] eq "Hazy Sunshine" || $values[0] eq "Hazy Sunshine" || $values[0] eq "Hot")
+curl -s "http://rss.accuweather.com/rss/liveweather_rss.asp?metric=${metric}&locCode=$location" |
+    perl -ne 'use utf8; if (/Currently/) {chomp;/\<title\>Currently: (.*)?\<\/title\>/; my @values=split(":",$1); if( $values[0] eq "Sunny" || $values[0] eq "Mostly Sunny" || $values[0] eq "Partly Sunny" || $values[0] eq "Intermittent Clouds" || $values[0] eq "Hazy Sunshine" || $values[0] eq "Hazy Sunshine" || $values[0] eq "Hot")
 {my $sun = "";binmode(STDOUT, ":utf8");print "$sun";}
 
 if( $values[0] eq "Mostly Cloudy" || $values[0] eq "Cloudy" || $values[0] eq "Dreary (Overcast)" || $values[0] eq "Fog")
@@ -31,4 +25,3 @@ if($values[0] eq "Flurries" || $values[0] eq "Mostly Cloudy w/ Flurries" || $val
 if($values[0] eq "Clear" || $values[0] eq "Mostly Clear" || $values[0] eq "Partly Cloudy"|| $values[0] eq "Intermittent Clouds"|| $values[0] eq "Hazy Moonlight"|| $values[0] eq "Mostly Cloudy"|| $values[0] eq "Partly Cloudy w/ Showers"|| $values[0] eq "Mostly Cloudy w/ Showers"|| $values[0] eq "Partly Cloudy w/ T-Storms"|| $values[0] eq "Mostly Cloudy w/ Flurries" || $values[0] eq "Mostly Cloudy w/ Snow")
 {my $night = "";binmode(STDOUT, ":utf8");print "$night";}
 print"$values[1]"; }'
-
