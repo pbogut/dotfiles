c0b05a6595380467568f70f41828c57fc244e861
Pawel Bogut <pbogut@pbogut.me>
2020-08-05 21:18:02 +0200

    Delete phpbrew

--- zshrc.d/phpbrew.zsh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#=================================================
-# name:   phpbrew.zsh
-# author: Pawel Bogut <https://pbogut.me>
-# date:   15/01/2019
-#=================================================
-source ~/.phpbrew/bashrc
