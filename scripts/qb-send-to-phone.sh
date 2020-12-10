13fc04bf413e71c323ac3cf745a825eedf5590b0
Pawel Bogut <pbogut@pbogut.me>
2020-12-10 22:47:30 +0100

    Add qb userscripts to connect with remarkable and phone

---
+++ scripts/qb-send-to-phone.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#=================================================
+# name:   qb-send-to-phone.sh
+# author: Pawel Bogut <https://pbogut.me>
+# date:   04/12/2020
+#=================================================
+api_url='https://autoremotejoaomgcd.appspot.com/sendmessage'
+key=$AUTO_REMOTE_KEY
+url=$QUTE_URL
+
+curl "$api_url" --data-urlencode "message=open-url=:=$url" --data-urlencode "key=$key"
+echo ':message-info "Sent to phone"' >> $QUTE_FIFO
