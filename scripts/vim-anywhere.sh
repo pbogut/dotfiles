d70f2af804e6b5aab2fcd204bb0e404ca995629e
Pawel Bogut <pbogut@pbogut.me>
2020-12-18 22:18:33 +0100

    Add draft option to vim-anywhere

--- scripts/vim-anywhere.sh
+++ scripts/vim-anywhere.sh
@@ -11,4 +11,20 @@ fi
 if [[ $1 == "--clipboard" ]]; then
     xsel -b > $file
 fi
+if [[ $1 == "--draft" ]]; then
+    xsel -b > $file
+    ext=$((
+        echo 'md'
+        echo 'sql'
+        echo 'json'
+        echo 'js'
+        echo 'php'
+        echo 'rb'
+        echo 'py'
+        echo 'exs'
+        ) | rofi -dmenu -p 'ext')
+    cd ~/Drafts
+    rand=$(head -n1 /dev/urandom | sha256sum | sed 's,^\(.\{8\}\).*,\1,g')
+    file="$(date +'%Y-%m-%d_%H:%M:%S')_$rand.$ext"
+fi
 urxvt -e nvim $file && cat $file | xsel -b
