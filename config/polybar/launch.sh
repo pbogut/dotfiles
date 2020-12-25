0b5badbbd31dd57ad211ea43629be3a49495f0eb
Pawel Bogut <pbogut@pbogut.me>
2020-12-25 16:56:14 +0100

    Fix single monitor launch of polybar

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -11,20 +11,20 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 
 
 screens=$(polybar -m | wc -l)
+primary=$(polybar -m | grep '(primary)' | awk '{gsub(/:$/, "", $1); print $1}')
 
 if [[ $screens -eq 2 ]]; then
-  primary=$(polybar -m | grep '(primary)' | awk '{gsub(/:$/, "", $1); print $1}')
+  echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
   secondary=$(polybar -m | grep -v '(primary)' | awk '{gsub(/:$/, "", $1); print $1}' | head -n1)
 
   MONITOR=$primary    polybar primary     2>&1 | tee -a /tmp/polybar1.log & disown
   MONITOR=$secondary  polybar secondary   2>&1 | tee -a /tmp/polybar2.log & disown
 else
-  polybar primary 2>&1 | tee -a /tmp/polybar1.log & disown
+  echo "---" | tee -a /tmp/polybar1.log
+  MONITOR=$primary    polybar primary     2>&1 | tee -a /tmp/polybar1.log & disown
 fi
 
 echo "Bars launched..."
