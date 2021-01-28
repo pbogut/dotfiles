a27115f17cb2b452b3a04cd7250bf1dddc687ea5
Pawel Bogut <pbogut@pbogut.me>
2021-01-29 00:36:55 +0100

    Update font settings

--- input-nerd-font.sh
+++ input-nerd-font.sh
@@ -7,7 +7,7 @@
 tmpdir=$(mktemp -d)
 cd $tmpdir
 if [[ ! -f $HOME/.cache/Input_Fonts.zip ]]; then
-  curl 'https://input.fontbureau.com/build/?fontSelection=fourStyleFamily&regular=InputMonoNarrow-Light&italic=InputMonoNarrow-LightItalic&bold=InputMonoNarrow-Medium&boldItalic=InputMonoNarrow-MediumItalic&a=0&g=0&i=serifs_round&l=serifs_round&zero=0&asterisk=0&braces=0&preset=default&line-height=1.2&accept=I+do&email=' -o $HOME/.cache/Input_Fonts.zip
+  curl 'https://input.fontbureau.com/build/?fontSelection=whole&a=0&g=0&i=serifs_round&l=serifs_round&zero=0&asterisk=0&braces=0&preset=default&line-height=1.2&accept=I+do&email=' -o $HOME/.cache/Input_Fonts.zip
 fi
 echo $tmpdir
 cp $HOME/.cache/Input_Fonts.zip ./
@@ -17,19 +17,11 @@ unzip Input_Fonts.zip
 git cl ryanoasis/nerd-fonts || cd $HOME/Projects/github.com/ryanoasis/nerd-fonts && git pull
 cd $tmpdir
 
-ls Input_Fonts/Input/ | while read font; do
-  options="--complete"
-  options="--fontawesome --fontawesomeextension --fontlinux --octicons --powersymbols --pomicons --powerline --powerlineextra --material --weather"
-  options="--fontawesome --fontawesomeextension --fontlinux --octicons --powersymbols"
-  options="--pomicons --powerline --powerlineextra"
-  options="--material --weather"
-  options="--material"
-  options="--complete --careful"
-  options="--mono --careful --fontawesome --fontawesomeextension --fontlinux --octicons --powersymbols --pomicons --powerline --powerlineextra --material --weather"
-
+find Input_Fonts/ -iname '*.ttf' | while read font; do
   options="--mono --complete --careful"
-  $HOME/Projects/github.com/ryanoasis/nerd-fonts/font-patcher $options "Input_Fonts/Input/$font" -out ~/.fonts/
+  $HOME/Projects/github.com/ryanoasis/nerd-fonts/font-patcher $options "$font" -out ~/.fonts/
   options="--complete --careful"
-  $HOME/Projects/github.com/ryanoasis/nerd-fonts/font-patcher $options "Input_Fonts/Input/$font" -out ~/.fonts/
+  $HOME/Projects/github.com/ryanoasis/nerd-fonts/font-patcher $options "$font" -out ~/.fonts/
 done
+
 fc-cache -f
