0e7b666fa7df7eda94553fc4ddcd63126354ec87
Pawel Bogut <pbogut@pbogut.me>
2021-02-14 22:17:59 +0100

    Add diff icons

--- config/nvim/lua/settings/icons.lua
+++ config/nvim/lua/settings/icons.lua
@@ -1,8 +1,15 @@
 local g = vim.g
 
 g.icon = {
+  -- lint
   error = '',
   warning = '',
   info = 'כֿ',
   hint = '',
+  -- diff
+  added = '',
+  ignored = '',
+  deleted = '',
+  changed = '',
+  renamed = '',
 }
