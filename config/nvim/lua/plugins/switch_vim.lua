b99145e40e6df70d253534a6e1fb15cc03734bb5
Pawel Bogut <pbogut@pbogut.me>
2021-02-16 21:35:15 +0100

    Update switch configuration
    Add laravel related switches
    Add word switches

--- config/nvim/lua/plugins/switch_vim.lua
+++ config/nvim/lua/plugins/switch_vim.lua
@@ -32,6 +32,15 @@ l.switch_php_scope = {
   ['\\<protected\\>'] = 'public',
   ['\\<public\\>'] =    'private',
 }
+l.switch_php_class_string = { -- one way ticket, why would you use string!?
+  ['create([\'"]\\\\\\=\\([a-zA-Z0-9_\\\\]*\\)[\'"])'] = 'create(\\\\\\1::class)',
+  ['make([\'"]\\\\\\=\\([a-zA-Z0-9_\\\\]*\\)[\'"])'] = 'make(\\\\\\1::class)',
+  ['get([\'"]\\\\\\=\\([a-zA-Z0-9_\\\\]*\\)[\'"])'] = 'get(\\\\\\1::class)',
+}
+l.switch_php_laravel_facade = {
+  ['use \\([A-Z][A-Za-z]*\\);'] = 'use Illuminate\\\\Support\\\\Facades\\\\\\1;',
+  ['use Illuminate\\\\Support\\\\Facades\\\\\\([A-Z][A-Za-z]*\\);'] = 'use \\1;',
+}
 l.switch_php_array = {
   ['\\<array(\\(.*\\))'] =      '[\\1]',
   ['\\(\\s*\\|^\\)\\[\\(.*\\)\\]'] = '\\1array(\\2)',
@@ -76,66 +85,87 @@ l.switch_diffline = {
   ['^-'] = ' ',
   ['^\\s'] = '+',
 }
+l.switch_words_tf = {
+  ['\\<true\\>'] =   'false',
+  ['\\<false\\>'] = 'true',
+}
+l.switch_words_lr = {
+  ['\\<left\\>'] =   'right',
+  ['\\<right\\>'] = 'left',
+}
+
+local function addDefinition(data)
+  local current = b.switch_custom_definitions or {}
+  b.switch_custom_definitions = u.merge_tables(current, data)
+end
 
 u.augroup('x_switch', {
     FileType = {
+      {'*', function()
+          addDefinition({
+            l.switch_words_tf,
+            l.switch_words_lr,
+          })
+      end},
       {'blade', function()
-          b.switch_custom_definitions = {
+          addDefinition({
             l.switch_blade_echo,
             l.switch_quotes,
-          }
+          })
       end},
       {'php', function()
-          b.switch_custom_definitions = {
+          addDefinition({
             l.switch_c_like_if,
             l.switch_c_like_while,
+            l.switch_php_class_string,
             l.switch_php_array,
             l.switch_php_comment,
             l.switch_php_scope,
             l.switch_php_magento_dispatch_event,
+            l.switch_php_laravel_facade,
             l.switch_quotes,
-          }
+          })
       end},
       {'javascript', function()
-          b.switch_custom_definitions = {
+          addDefinition({
             l.switch_c_like_if,
             l.switch_quotes,
-          }
+          })
       end},
       {'javascript.jsx', function()
-          b.switch_custom_definitions = {
+          addDefinition({
             l.switch_c_like_if,
             l.switch_quotes,
-          }
+          })
       end},
       {'elixir', function()
-          b.switch_custom_definitions = {
+          addDefinition({
             l.switch_elixir_assert,
             l.switch_elixir_map,
             l.switch_quotes,
-          }
+          })
       end},
       {'markdown,md', function()
-          b.switch_custom_definitions = {
+          addDefinition({
             l.switch_md_checkbox,
             l.switch_quotes,
-          }
+          })
       end},
       {'vimwiki', function()
-          b.switch_custom_definitions = {
+          addDefinition({
             l.switch_vimwiki_checkbox,
             l.switch_quotes,
-          }
+          })
       end},
       {'email', function()
-          b.switch_custom_definitions = {
+          addDefinition({
             l.switch_quotes,
-          }
+          })
       end},
       {'diff', function()
-          b.switch_custom_definitions = {
+          addDefinition({
             l.switch_diffline ,
-          }
+          })
       end},
     }
 })
