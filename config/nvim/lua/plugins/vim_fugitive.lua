f1298c0ab271f4b10ceea2902d130bdcfb44ab5b
Pawel Bogut <pbogut@pbogut.me>
2021-02-17 20:04:52 +0100

    Open Gst in split, if no split then create new one

--- config/nvim/lua/plugins/vim_fugitive.lua
+++ config/nvim/lua/plugins/vim_fugitive.lua
@@ -2,7 +2,20 @@ local u = require('utils')
 local cmd = vim.cmd
 local fn = vim.fn
 
-u.command('Gst', '0Gstatus')
+u.command('Gst', function()
+  local layout = fn.winlayout()
+  if layout[1] == 'leaf' then
+    -- if single window do split
+    cmd('wincmd v')
+    cmd('enew')
+    cmd('wincmd h')
+  elseif layout[2] and #layout[2] == 2 then
+    -- if splited already move to left
+    cmd('wincmd H')
+  end
+
+  cmd('0Gstatus')
+end)
 u.command('Gl', '0Glog')
 
 u.command('Grevert', function()
