07e3a9ac320d4179d09a78983c19332eb5901c5f
Pawel Bogut <pbogut@pbogut.me>
2021-05-21 10:25:28 +0200

    Add php base template

---
+++ config/nvim/templates/file.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @class1 [[php.class1]]
+ * @class  [[php.class]]
+ * @ns     [[php.namespace]]
+ * @name   [[base_name]]
+ * @author [[author]] <[[author_contact]]>
+ * @date   [[current_date]]
+ */
+[[_]]
