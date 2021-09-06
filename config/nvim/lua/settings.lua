fe859e71a2a768205ff67030cf256012d07f48d2
Pawel Bogut <pbogut@pbogut.me>
2021-09-06 12:21:17 +0200

    Update settings to use new opt field

--- config/nvim/lua/settings.lua
+++ config/nvim/lua/settings.lua
@@ -5,7 +5,7 @@ require('settings.icons')
 require('settings.highlights')
 
 local cmd = vim.cmd
-local o = vim.o
+local o = vim.opt
 local wo = vim.wo
 local bo = vim.bo
 
@@ -17,38 +17,28 @@ cmd('set runtimepath+=~/.vim')
 cmd('silent! colorscheme solarized')
 
 o.list = true
-wo.list = true
 o.updatetime = 100
 o.signcolumn = 'yes'
-wo.signcolumn = 'yes'
 o.showmode = false
 o.ttimeoutlen = 0   -- eliminate esc timeout
 o.report = 0
 o.hlsearch = false
 o.cursorline = true
-wo.cursorline = true
 o.cursorcolumn = true
-wo.cursorcolumn = true
 o.completeopt = 'menuone,noselect,noinsert'
 o.cmdheight = 2
 o.tabstop = 2
-bo.tabstop = 2
 o.shiftwidth = 2
-bo.shiftwidth = 2
 o.expandtab = true
-bo.expandtab = true
 o.scrolloff = 3
-wo.scrolloff = 3
 o.number = true
-wo.number = true
 o.relativenumber = true
-wo.relativenumber = true
 o.lazyredraw = true
 o.wildmenu = true
 o.showcmd = true
 o.undodir = os.getenv("HOME") .. '/.vim/undofiles//'
 o.undofile = true
-o.listchars = 'tab:▸ ,eol:¬,trail:⋅,extends:❯,precedes:❮,nbsp:%'
+o.listchars = 'tab:▸,eol:¬,trail:⋅,extends:❯,precedes:❮,nbsp:%'
 o.showbreak = '↪'
 o.history = 10000
 o.undolevels = 1000
@@ -57,11 +47,8 @@ o.hidden = true
 o.clipboard = 'unnamedplus'
 
 o.colorcolumn = '81' -- line 80 limit 81 is colored
-wo.colorcolumn = '81' -- line 80 limit 81 is colored
 o.foldmethod = 'manual'
-wo.foldmethod = 'manual'
 o.foldnestmax = 10
-wo.foldnestmax = 10
 o.foldlevelstart = 99
 
 o.termguicolors = true
@@ -72,7 +59,7 @@ o.shell='/bin/bash'
 o.inccommand = 'split'
 -- rip grep as default grep program
 o.grepprg = 'rg --vimgrep --no-heading'
-bo.grepprg = 'rg --vimgrep --no-heading'
+-- bo.grepprg = 'rg --vimgrep --no-heading'
 o.grepformat = '%f:%l:%c:%m,%f:%l:%m'
 
 
