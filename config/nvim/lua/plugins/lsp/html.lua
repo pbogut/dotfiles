4e191bcc8284960af493a9bbc8156bf2cd17f962
Pawel Bogut <pbogut@pbogut.me>
2021-09-06 12:17:23 +0200

    Fix html language server cmd

--- config/nvim/lua/plugins/lsp/html.lua
+++ config/nvim/lua/plugins/lsp/html.lua
@@ -3,7 +3,8 @@ local me = {}
 
 function me.setup(opts)
   opts = vim.tbl_deep_extend('keep', opts, {
-    filetypes = { "html", "php", "blade" }
+    filetypes = { "html", "php", "blade" },
+    cmd = { "html-languageserver", '--stdio' },
   })
   lspconfig.html.setup(opts)
 end
