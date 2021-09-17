8057140115eb7ba124fe393499507856d890073a
Pawel Bogut <pbogut@pbogut.me>
2021-09-17 20:04:45 +0200

    Attempt to fix php indentation while using treesitter

--- config/nvim/lua/plugins/nvim_treesitter.lua
+++ config/nvim/lua/plugins/nvim_treesitter.lua
@@ -2,6 +2,7 @@ require'nvim-treesitter.configs'.setup {
   ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   highlight = {
     enable = true, -- its buggy, I'm on fence with it
+    additional_vim_regex_highlighting = {'php'}
   },
   indent = {
     enable = true, -- like it better in php/html so far, but its shit in pure php
