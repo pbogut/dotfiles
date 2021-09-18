f3c71509095be4a55558f09fe6a7296f605cb8d0
Pawel Bogut <pbogut@pbogut.me>
2021-09-18 11:58:02 +0200

    Placeholders for elixir templates

---
+++ config/nvim/lua/template/placeholder/elixir.lua
@@ -0,0 +1,51 @@
+local h = require('projector.helper')
+
+local p = {}
+
+p.module_name_parts = {
+  value = function()
+    local list = h.get_file_parts()
+    if list[1] == 'lib' or list[1] == 'test' then
+      table.remove(list, 1)
+    end
+
+    if list[1]:match('%_web$') then
+      local skip = {
+        -- phoenix
+        'controllers',
+        'views',
+        'channels',
+        'live',
+        -- for tests
+        'features'
+      }
+
+      local drop = false
+      for _, s in pairs(skip) do
+        if list[2] == s then
+          drop = true
+        end
+      end
+      if drop then
+        table.remove(list, 2)
+      end
+    end
+
+    return h.map(h.map(list, h.camelcase), h.upper_first)
+  end
+}
+
+p.module_name = {
+  value = function()
+    return table.concat(p.module_name_parts.value(), '.')
+  end
+}
+
+p.base_module = {
+  value = function()
+    return p.module_name_parts.value()[1]
+  end
+}
+
+print(vim.inspect(p.base_module.value()))
+return p
