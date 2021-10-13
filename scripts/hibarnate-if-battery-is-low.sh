eb17bc3bb890a31bb766b50b574b15132d0e699c
Pawel Bogut <pbogut@pbogut.me>
2021-10-13 14:47:34 +0200

    Add daemon mode to hibernate script

--- scripts/hibarnate-if-battery-is-low.sh
+++ scripts/hibarnate-if-battery-is-low.sh
@@ -7,18 +7,32 @@
 icon="/usr/share/icons/gnome/32x32/devices/battery.png"
 limit="4"
 notify="5"
+interval=60
 
-battery="$(acpi | sed -E 's,^.*? ([0-9]+)%.*?$,\1,')"
+usage() {
+  echo "Ussage: ${0##*/} [OPTIONS]"
+  echo ""
+  echo "Options:"
+  echo "  -D, --daemon      run in daemon mode"
+  echo "  -i, --interval    delay between each check in daemon mode"
+  echo ""
+  echo "  -h, --help        display this help and exit"
+}
 
-if [[ $notify -gt $battery ]] || [[ $notify -eq $battery ]]; then
+battery_level() {
+  battery="$(acpi | sed -E 's,^.*? ([0-9]+)%.*?$,\1,')"
+  echo "$battery"
+}
+
+notify() {
   dunstify -I $icon \
-    "Low battery, pleas charge or system will be hibernated when battery level drops to $limit%"
-fi
+    "Low battery level ($battery%), please charge or system will be hibernated when battery level drops to $limit%"
+}
 
-if [[ $limit -gt $battery ]] || [[ $limit -eq $battery ]]; then
+hibernate() {
   yad \
     --title="Hibernating..." \
-    --text "Battery level: $battery%\n\nSystem is going to be hibernated.\n\nClick cancel to prevent it. " \
+    --text "Battery level: $battery%\n\nSystem is going to be hibernated.\n\nClick cancel to prevent it." \
     --image $icon \
     --question \
     --timeout=30 \
@@ -27,9 +41,53 @@ if [[ $limit -gt $battery ]] || [[ $limit -eq $battery ]]; then
   result=$?
 
   # if cancel clicked or esc hit
-  if [[ $result -eq 1 ]] || [[ $result -eq 252 ]]; then
-    exit 1
-  else
-    sudo systemctl hibernate
+  if [[ ! $result -eq 1 ]] && [[ ! $result -eq 252 ]]; then
+    systemctl hibernate
   fi
+}
+
+check_notification() {
+  battery=$(battery_level)
+  if [[ $notify -gt $battery ]] || [[ $notify -eq $battery ]]; then
+    notify
+  fi
+  if [[ $limit -gt $battery ]] || [[ $limit -eq $battery ]]; then
+    hibernate
+  fi
+}
+
+while test $# -gt 0; do
+  case "$1" in
+    --interval|--interval=*|-i)
+      if [[ $1 =~ --[a-z]+= ]]; then
+        _val="${1//--interval=/}"
+        shift
+      else
+        _val="$2"
+        shift; shift
+      fi
+      interval="$_val"
+      ;;
+    --daemon|-D)
+      daemon=1
+      shift
+      ;;
+    --help|-h)
+      usage
+      exit 0
+      ;;
+    *)
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+if [[ $daemon -eq 1 ]]; then
+  while :; do
+    check_notification
+    sleep "${interval}s";
+  done
 fi
+
+check_notification
