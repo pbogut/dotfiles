1554b67a4c4e730bb800804171258def7f2d6168
Pawel Bogut <pbogut@pbogut.me>
2021-11-08 19:53:54 +0100

    Barrier wrapper for manage xcape settings

---
+++ scripts/barrierc-xcape.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#=================================================
+# name:   barrierc-xcape
+# author: Pawel Bogut <https://pbogut.me>
+# date:   23/10/2021
+#=================================================
+barrierc --disable-crypto --no-daemon "$1" | while read line; do
+if [[ $line =~ "entering screen" ]]; then
+  killall xcape -9
+fi
+if [[ $line =~ "leaving screen" ]]; then
+    xcape -e 'Control_L=Escape'
+    xcape -e 'Shift_L=parenleft'
+    xcape -e 'Shift_R=parenright'
+fi
+done
