4af706f1bee1d19e6c8b79d01946cc9d2d3104bf
Pawel Bogut <pbogut@pbogut.me>
2021-12-18 17:58:29 +0100

    feat(null-ls): Add shfmt formater + stylua format

--- config/nvim/lua/plugins/null_ls_nvim.lua
+++ config/nvim/lua/plugins/null_ls_nvim.lua
@@ -8,8 +8,20 @@ local cfg = { sources = {}, on_attach = on_attach }
 
 local sources = {
   -- lua
-  { builtin = null_ls.builtins.diagnostics.luacheck, enabled = false },
-  { builtin = null_ls.builtins.formatting.stylua },
+  {
+    builtin = null_ls.builtins.diagnostics.luacheck,
+    enabled = false,
+  },
+  {
+    builtin = null_ls.builtins.formatting.stylua,
+    enabled = true,
+  },
+  -- bash
+  {
+    builtin = null_ls.builtins.formatting.shfmt,
+    args = { '-i', '2', '-filename', '$FILENAME' },
+    enabled = true,
+  },
   -- php
   {
     builtin = null_ls.builtins.diagnostics.phpcs,
@@ -18,16 +30,32 @@ local sources = {
       return vim.fn.filereadable(vim.fn.getcwd() .. '/phpcs.xml') == 1
     end,
   },
-  { builtin = null_ls.builtins.formatting.phpcbf, enabled = false },
+  {
+    builtin = null_ls.builtins.formatting.phpcbf,
+    enabled = false,
+  },
   -- python
-  { builtin = null_ls.builtins.diagnostics.flake8, enabled = true },
-  { builtin = null_ls.builtins.formatting.isort, enabled = true },
-  { builtin = null_ls.builtins.formatting.yapf, enabled = true },
+  {
+    builtin = null_ls.builtins.diagnostics.flake8,
+    enabled = true,
+  },
+  {
+    builtin = null_ls.builtins.formatting.isort,
+    enabled = true,
+  },
+  {
+    builtin = null_ls.builtins.formatting.yapf,
+    enabled = true,
+  },
   -- other
-  { builtin = null_ls.builtins.completion.spell, enabled = false },
+  {
+    builtin = null_ls.builtins.completion.spell,
+    enabled = false,
+  },
   {
     builtin = null_ls.builtins.formatting.prettier,
     filetypes = { 'markdown' },
+    enabled = true,
   },
 }
 
@@ -75,6 +103,9 @@ for _, source in ipairs(sources) do
   end
   if source.filetypes then
     source.builtin.filetypes = source.filetypes
+  end
+  if source.args then
+    source.builtin._opts.args = source.args
   end
   if
     source.enabled == nil
