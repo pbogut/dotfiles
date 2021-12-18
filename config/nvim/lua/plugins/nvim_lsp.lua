1ac6dfde13f5aa507c6bc558a587fb07a76bd31a
Pawel Bogut <pbogut@pbogut.me>
2021-12-18 21:54:01 +0100

    feat(lsp): Update lsp config + format with stylua

--- config/nvim/lua/plugins/nvim_lsp.lua
+++ config/nvim/lua/plugins/nvim_lsp.lua
@@ -1,4 +1,4 @@
-local u = require'utils'
+local u = require('utils')
 local lspconfig = require('lspconfig')
 local has_lspstatus, lspstatus = pcall(require, 'lsp-status')
 local has_lsp_signature, lsp_signature = pcall(require, 'lsp_signature')
@@ -7,33 +7,67 @@ local cmd = vim.cmd
 local g = vim.g
 local no_lsp_bind = '<cmd>lua print("No LSP attached")<CR>'
 
-local signature_action = ''
-if has_lsp_signature then
-  signature_action = '<cmd>lua require"lsp_signature".toggle_float_win()<CR>'
-else
-  signature_action = '<cmd>lua vim.lsp.buf.signature_help()<CR>'
+local signature_action = crequire('lsp_signature', {
+  done = function()
+    return '<cmd>lua require"lsp_signature".toggle_float_win()<CR>'
+  end,
+  fail = function()
+    return '<cmd>lua vim.lsp.buf.signature_help()<CR>'
+  end,
+})
+
+local vanilla = {
+  definition = 'lua vim.lsp.buf.definition()',
+  implementation = 'lua vim.lsp.buf.implementation()',
+  type_definition = 'lua vim.lsp.buf.type_definition()',
+  references = 'lua vim.lsp.buf.references()',
+  document_symbol = 'lua vim.lsp.buf.document_symbol()',
+  workspace_symbol = 'lua vim.lsp.buf.workspace_symbol()',
+  code_action = 'lua vim.lsp.buf.code_action()',
+}
+
+local telescope = {
+  definition = 'Telescope lsp_definitions',
+  implementation = 'Telescope lsp_implementations',
+  type_definition = 'Telescope lsp_type_definitions',
+  references = 'Telescope lsp_references',
+  document_symbol = 'Telescope lsp_document_symbols',
+  workspace_symbol = 'Telescope lsp_workspace_symbols',
+  code_action = 'Telescope lsp_code_actions',
+}
+
+local function maybe_telescope(name)
+  return function()
+    if vim.fn.exists(':Telescope') then
+      vim.cmd(telescope[name])
+    else
+      vim.cmd(vanilla[name])
+    end
+  end
 end
+
 local bindings = {
-  {'n', '<space>er', ':TroubleToggle lsp_references<CR>', no_lsp_bind},
-  {'n', ']d', ':lua vim.diagnostic.goto_next()<CR>', no_lsp_bind},
-  {'n', '[d', ':lua vim.diagnostic.goto_prev()<CR>', no_lsp_bind},
-  {'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', false},
-  {'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', false},
-  {'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', false},
-  {'n', '<C-k>', signature_action, no_lsp_bind},
-  {'i', '<C-k>', signature_action, no_lsp_bind},
-  {'n', '<space>i', '<cmd>lua vim.lsp.buf.implementation()<CR>', no_lsp_bind},
-  {'n', '<space>T', '<cmd>lua vim.lsp.buf.type_definition()<CR>', no_lsp_bind},
-  {'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', no_lsp_bind, {silent=false}},
-  {'n', '<space>rr', '<cmd>lua vim.lsp.buf.references()<CR>', no_lsp_bind},
-  {'n', '<space>ee', '<cmd>lua vim.diagnostic.open_float(0, {scope = "line"})<CR>', no_lsp_bind},
-  {'n', '<space>sd', '<cmd>lua vim.lsp.buf.document_symbol()<CR>', no_lsp_bind},
-  {'n', '<space>sD', '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>', no_lsp_bind},
-  {'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', no_lsp_bind},
+  { 'n', ']d', ':lua vim.diagnostic.goto_next()<CR>', no_lsp_bind },
+  { 'n', '[d', ':lua vim.diagnostic.goto_prev()<CR>', no_lsp_bind },
+  { 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', false },
+  { 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', false },
+  { 'n', '<C-k>', signature_action, no_lsp_bind },
+  { 'i', '<C-k>', signature_action, no_lsp_bind },
+
+  { 'n', 'gd', maybe_telescope('definition'), false },
+  { 'n', '<space>i', maybe_telescope('implementation'), no_lsp_bind },
+  { 'n', '<space>T', maybe_telescope('type_definition'), no_lsp_bind },
+  { 'n', '<space>rr', maybe_telescope('references'), no_lsp_bind },
+  { 'n', '<space>sd', maybe_telescope('document_symbol'), no_lsp_bind },
+  { 'n', '<space>sD', maybe_telescope('workspace_symbol'), no_lsp_bind },
+  { 'n', '<space>ca', maybe_telescope('code_action'), no_lsp_bind },
+
+  { 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', no_lsp_bind, { silent = false } },
+  { 'n', '<space>ee', '<cmd>lua vim.diagnostic.open_float(0, {scope = "line"})<CR>', no_lsp_bind },
   -- custom format functions with fall back to Neoformat cmd
-  {'n', '<space>af', '<cmd>lua vim.lsp.buf.formatting()<CR>', no_lsp_bind},
-  {'v', '<space>af', ':lua vim.lsp.buf.range_formatting()<CR>', no_lsp_bind},
-  {'x', '<space>af', ':lua vim.lsp.buf.range_formatting()<CR>', no_lsp_bind},
+  { 'n', '<space>af', '<cmd>lua vim.lsp.buf.formatting()<CR>', no_lsp_bind },
+  { 'v', '<space>af', ':lua vim.lsp.buf.range_formatting()<CR>', no_lsp_bind },
+  { 'x', '<space>af', ':lua vim.lsp.buf.range_formatting()<CR>', no_lsp_bind },
 }
 
 -- prevent stupid errors when using mapping with no lsp attached
@@ -51,8 +85,8 @@ local on_attach = function(client, bufnr)
       floating_window = true,
       floating_window_above_cur_line = true,
       hint_enable = false,
-      handler_opts = { border = "none" },
-      toggle_key = '<c-k>'
+      handler_opts = { border = 'none' },
+      toggle_key = '<c-k>',
     })
   end
 
@@ -75,59 +109,17 @@ if has_lspstatus then
   lspstatus.register_progress()
 end
 
-local function location_handler(_, result, ctx)
-  vim.g.lsp_last_word = vim.fn.expand('<cword>')
-  if result == nil or vim.tbl_isempty(result) then
-    print(ctx.method, 'No location found')
-    return nil
-  end
-  local util = require('vim.lsp.util')
-  if vim.tbl_islist(result) then
-    if #result == 1 then
-      util.jump_to_location(result[1])
-    elseif #result > 1 then
-      vim.fn.setqflist(util.locations_to_items(result))
-      require('plugins.fzf').quickfix(vim.fn.expand('<cword>'));
-      -- api.nvim_command("copen")
-      -- api.nvim_command("wincmd p")
-    end
-  else
-    util.jump_to_location(result)
-  end
-end
-
-vim.lsp.handlers['textDocument/declaration'] = location_handler
-vim.lsp.handlers['textDocument/definition'] = location_handler
-vim.lsp.handlers['textDocument/typeDefinition'] = location_handler
-vim.lsp.handlers['textDocument/implementation'] = location_handler
-vim.lsp.handlers['textDocument/references'] = function(_, result)
-  vim.g.lsp_last_word = vim.fn.expand('<cword>')
-  if not result then return end
-  if #result == 0 then
-    cmd('echo "Getting references..."')
-    return
-  end
-  cmd('echo ""')
-  local util = require('vim.lsp.util')
-  vim.fn.setqflist(util.locations_to_items(result))
-  require('plugins.fzf').quickfix(vim.fn.expand('<cword>'));
-  -- api.nvim_command("copen")
-  -- api.nvim_command("wincmd p")
-end
-
-vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    -- Enable underline, use default values
-    underline = true,
-    -- Enable virtual text, override spacing to 8
-    virtual_text = {
-      spacing = 4,
-      prefix = '■',
-    },
-    -- Disable a feature
-    update_in_insert = false,
-  }
-)
+vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  -- Enable underline, use default values
+  underline = true,
+  -- Enable virtual text, override spacing to 8
+  virtual_text = {
+    spacing = 4,
+    prefix = '■',
+  },
+  -- Disable a feature
+  update_in_insert = false,
+})
 
 u.highlights({
   FloatBorder = { guibg = g.colors.base0, guifg = vim.g.colors.base03 },
@@ -147,31 +139,31 @@ u.highlights({
 })
 
 u.signs({
-  DiagnosticSignError = { text = g.icon.error, texthl = "DiagnosticSignError" },
-  DiagnosticSignWarn = { text = g.icon.warning, texthl = "DiagnosticSignWarn" },
-  DiagnosticSignInfo = { text = g.icon.info, texthl = "DiagnosticSignInfo" },
-  DiagnosticSignHint = { text = g.icon.hint, texthl = "DiagnosticSignHint" },
+  DiagnosticSignError = { text = g.icon.error, texthl = 'DiagnosticSignError' },
+  DiagnosticSignWarn = { text = g.icon.warning, texthl = 'DiagnosticSignWarn' },
+  DiagnosticSignInfo = { text = g.icon.info, texthl = 'DiagnosticSignInfo' },
+  DiagnosticSignHint = { text = g.icon.hint, texthl = 'DiagnosticSignHint' },
 })
 
-lspconfig.vimls.setup {on_attach = on_attach}
-lspconfig.vuels.setup {on_attach = on_attach}
-lspconfig.bashls.setup {on_attach = on_attach}
-lspconfig.gopls.setup {on_attach = on_attach}
-lspconfig.cssls.setup {on_attach = on_attach, capabilities = capabilities}
+lspconfig.vimls.setup({ on_attach = on_attach })
+lspconfig.vuels.setup({ on_attach = on_attach })
+lspconfig.bashls.setup({ on_attach = on_attach })
+lspconfig.gopls.setup({ on_attach = on_attach })
+lspconfig.cssls.setup({ on_attach = on_attach, capabilities = capabilities })
 -- lspconfig.pyright.setup {on_attach = on_attach}
-lspconfig.jedi_language_server.setup {on_attach = on_attach}
-lspconfig.dockerls.setup {on_attach = on_attach}
-lspconfig.solargraph.setup {on_attach = on_attach}
+lspconfig.jedi_language_server.setup({ on_attach = on_attach })
+lspconfig.dockerls.setup({ on_attach = on_attach })
+lspconfig.solargraph.setup({ on_attach = on_attach })
 -- require 'plugins.lsp.denols'.setup {on_attach = on_attach}
-require 'plugins.lsp.tsserver'.setup{on_attach = on_attach}
-require 'plugins.lsp.emmet_ls'.setup {on_attach = on_attach, capabilities = capabilities}
-require 'plugins.lsp.tailwindcss'.setup {on_attach = on_attach, capabilities = capabilities}
-require 'plugins.lsp.sumneko_lua'.setup {on_attach = on_attach, capabilities = capabilities}
-require 'plugins.lsp.intelephense'.setup {on_attach = on_attach, capabilities = capabilities}
-require 'plugins.lsp.jsonls'.setup {on_attach = on_attach, capabilities = capabilities}
-require 'plugins.lsp.html'.setup {on_attach = on_attach, capabilities = capabilities}
-require 'plugins.lsp.elixirls'.setup {on_attach = on_attach, capabilities = capabilities}
-require 'plugins.lsp.prosemd'.setup {on_attach = on_attach}
+require('plugins.lsp.tsserver').setup({ on_attach = on_attach })
+require('plugins.lsp.emmet_ls').setup({ on_attach = on_attach, capabilities = capabilities })
+require('plugins.lsp.tailwindcss').setup({ on_attach = on_attach, capabilities = capabilities })
+require('plugins.lsp.sumneko_lua').setup({ on_attach = on_attach, capabilities = capabilities })
+require('plugins.lsp.intelephense').setup({ on_attach = on_attach, capabilities = capabilities })
+require('plugins.lsp.jsonls').setup({ on_attach = on_attach, capabilities = capabilities })
+require('plugins.lsp.html').setup({ on_attach = on_attach, capabilities = capabilities })
+require('plugins.lsp.elixirls').setup({ on_attach = on_attach, capabilities = capabilities })
+-- require 'plugins.lsp.prosemd'.setup {on_attach = on_attach}
 
 local function get_active_client_map()
   local client_list = {}
@@ -179,7 +171,7 @@ local function get_active_client_map()
     local root_dir = client.config.root_dir
     local filetypes = client.config.filetypes
     local client_id = client.id
-    for _, file_type  in ipairs(filetypes) do
+    for _, file_type in ipairs(filetypes) do
       if nil == client_list[file_type] then
         client_list[file_type] = {}
       end
@@ -210,11 +202,11 @@ local function attach_lsp_to_new_buffer()
 end
 
 u.command('LspReload', function()
-     vim.lsp.stop_client(vim.lsp.get_active_clients())
-     cmd('edit')
+  vim.lsp.stop_client(vim.lsp.get_active_clients())
+  cmd('edit')
 end)
 u.command('LspAttachBuffer', attach_lsp_to_new_buffer)
 
 return {
-  on_attach = on_attach
+  on_attach = on_attach,
 }
