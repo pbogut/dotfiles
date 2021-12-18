df57b4c9dd4b53cbf12bb90ae22c316501e08577
Pawel Bogut <pbogut@pbogut.me>
2021-12-19 00:14:44 +0100

    feat(barrier): Autostart without encryption

--- redeye.autostart.sh
+++ redeye.autostart.sh
@@ -4,5 +4,5 @@
 # author: Pawel Bogut <https://pbogut.me>
 # date:   09/08/2018
 #=================================================
-rerun barriers barriers
+rerun barriers "barriers --disable-crypto"
 rerun picom "picom -b --xrender-sync-fence"
