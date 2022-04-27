bac9f098646f5fab6ca91b7182c6e33048c77846
Pawel Bogut <pbogut@pbogut.me>
2022-04-27 22:03:10 +0200

    feat(flatpak): Add flatpak install file

---
+++ flatpak.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#=================================================
+# name:   flatpak
+# author: Pawel Bogut <https://pbogut.me>
+# date:   27/04/2022
+#=================================================
+flatpak install com.discordapp.Discord
+flatpak install com.getferdi.Ferdi
+flatpak install com.github.tchx84.Flatseal
+flatpak install com.heroicgameslauncher.hgl
+flatpak install com.valvesoftware.Steam
+flatpak install com.valvesoftware.SteamLink
+flatpak install io.github.trigg.discover_overlay
+flatpak install net.fsuae.FS-UAE
+flatpak install net.lutris.Lutris
+flatpak install org.onlyoffice.desktopeditors
+flatpak install org.yuzu_emu.yuzu
+flatpak install re.chiaki.Chiaki
