79fc49dc20524fd2e40628d9a58f1ed986b0da74
Pawel Bogut <pbogut@pbogut.me>
2022-07-16 12:43:51 +0200

    fix: Fix todo rofi script

--- scripts/todo.sh
+++ scripts/todo.sh
@@ -8,11 +8,12 @@ _list() {
         todo list
         echo ":new"
 }
+
 selection=$(_list | tac | rofi -dmenu)
-id=$(echo $selection | sed 's/[[:space:]]*\([[:digit:]]*\).*/\1/g')
+id=$(echo $selection | sed 's/\[.\][[:space:]]*\([[:digit:]]*\).*/\1/g')
 
 if [[ $selection == ":new" ]]; then
-        urxvt -geometry 130x35 -T FLOATING_WINDOW -e zsh -ic "todo new"
+        $TERMINAL -t FLOATING_WINDOW -e zsh -ic "todo new"
 elif [[ -n $id ]]; then
-        urxvt -geometry 130x35 -T FLOATING_WINDOW -e zsh -ic "todo edit $id"
+        $TERMINAL -t FLOATING_WINDOW -e zsh -ic "todo edit $id"
 fi
