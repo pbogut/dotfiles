294795bb4f941ee7196b47252e4a0f3c3c4b6cf7
Pawel Bogut <pbogut@pbogut.me>
2023-03-01 17:50:03 +0100

    feat(nvim): harpoon file reformat

--- config/nvim/lua/plugins/harpoon.lua
+++ config/nvim/lua/plugins/harpoon.lua
@@ -1,22 +1,20 @@
 local k = vim.keymap
-local ui = require("harpoon.ui")
-local mark = require("harpoon.mark")
+local ui = require('harpoon.ui')
+local mark = require('harpoon.mark')
 
-require("harpoon").setup({
-    menu = {
-        width = math.floor(vim.api.nvim_win_get_width(0) * 0.8)
-    }
+require('harpoon').setup({
+  menu = {
+    width = math.floor(vim.api.nvim_win_get_width(0) * 0.8),
+  },
 })
 
-k.set('n', 'ŋ', ui.toggle_quick_menu)          -- ALT_GR-G
-k.set('n', '„', mark.add_file)                 -- ALT_GR+V
+k.set('n', 'ŋ', ui.toggle_quick_menu) -- ALT_GR-G
+k.set('n', '„', mark.add_file) -- ALT_GR+V
 k.set('n', 'æ', function() ui.nav_file(1) end) -- ALT_GR-F
 k.set('n', 'ð', function() ui.nav_file(2) end) -- ALT_GR-D
 k.set('n', 'ś', function() ui.nav_file(3) end) -- ALT_GR-S
 k.set('n', 'ą', function() ui.nav_file(4) end) -- ALT_GR-A
-
--- navigate to harpones by count motion
--- ie. 2h will call nav_file(2)
+-- navigate to harpones by count motion ie. 2h will call nav_file(2)
 k.set('n', 'h', function()
   if vim.v.count == 0 then
     local pos = vim.api.nvim_win_get_cursor(0)
