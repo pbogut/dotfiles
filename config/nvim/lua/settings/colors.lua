e59b860ccdf7715262be946cbdd361551647f5ee
Pawel Bogut <pbogut@pbogut.me>
2023-04-20 19:13:28 +0200

    feat(nvim): swap background colors

--- config/nvim/lua/settings/colors.lua
+++ config/nvim/lua/settings/colors.lua
@@ -5,8 +5,8 @@ local fn = vim.fn
 local cmd = vim.cmd
 
 g.colors = {
-  ad_dark1 = '#001c26',
-  ad_dark2 = '#00222b',
+  ad_dark1 = '#00222b',
+  ad_dark2 = '#001c26',
 
   ad_info = '#a68f46',
   ad_hint = '#9eab7d',
