a599aef7cc584c28247c4033907397dc633ecc5e
Pawel Bogut <pbogut@pbogut.me>
2023-04-20 19:01:10 +0200

    feat(nvim): allow quicklist navigation if trouble is not opened

--- config/nvim/lua/plugins/trouble_nvim.lua
+++ config/nvim/lua/plugins/trouble_nvim.lua
@@ -7,9 +7,17 @@ k.set('n', '<space>lq', '<cmd>TroubleToggle quickfix<cr>')
 k.set('n', '<space>ll', '<cmd>TroubleToggle loclist<cr>')
 
 k.set('n', ']q', function()
-  trouble.next({ skip_groups = true, jump = true })
+  if trouble.is_open() then
+    trouble.next({ skip_groups = true, jump = true })
+  else
+    pcall(vim.cmd.cnext)
+  end
 end)
 
 k.set('n', '[q', function()
-  trouble.previous({ skip_groups = true, jump = true })
+  if trouble.is_open() then
+    trouble.previous({ skip_groups = true, jump = true })
+  else
+    pcall(vim.cmd.cprevious)
+  end
 end)
