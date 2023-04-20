f5ebd9da0a3756fd5f59bc136703889a9b1ac742
Pawel Bogut <pbogut@pbogut.me>
2023-04-20 18:53:57 +0200

    feat(nvim): add command to clean up bookmarks for removed files

--- config/nvim/lua/plugins/vim_bookmarks.lua
+++ config/nvim/lua/plugins/vim_bookmarks.lua
@@ -1,3 +1,21 @@
 vim.g.bookmark_save_per_working_dir = 1
 vim.g.bookmark_sign = '📌'
 vim.g.bookmark_annotation_sign = '📔'
+
+
+vim.cmd([[
+  function! BookmarkClearForMissingFiles()
+    let files = bm#all_files()
+    for file in files
+      if !filereadable(file)
+        let lines = bm#all_lines(file)
+        for line_nr in lines
+          " call s:bookmark_remove(file, line_nr)
+          call bm#del_bookmark_at_line(file, line_nr)
+        endfor
+      endif
+    endfor
+    echo "Bookmarks for missing files removed"
+  endfunction
+  command! BookmarkClearForMissingFiles call BookmarkClearForMissingFiles()
+]]);
