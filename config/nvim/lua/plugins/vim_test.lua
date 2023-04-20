c74ffdcce7b5f8bde09baf12e730d0da6ef1ed78
Pawel Bogut <pbogut@pbogut.me>
2023-04-20 19:00:20 +0200

    feat(nvim): run test in background when debugger is on

--- config/nvim/lua/plugins/vim_test.lua
+++ config/nvim/lua/plugins/vim_test.lua
@@ -1,13 +1,30 @@
+local cfg = require('config')
 local k = vim.keymap
 local g = vim.g
+local has_dap, dap = pcall(require, 'dap')
 
 local function setup()
-  k.set('n', '<space>tn', '<cmd>TestNearest<cr>')
-  k.set('n', '<space>tf', '<cmd>TestFile<cr>')
-  k.set('n', '<space>ts', '<cmd>TestSuite<cr>')
-  k.set('n', '<space>tl', '<cmd>TestLast<cr>')
-  k.set('n', '<space>tt', '<cmd>TestLast<cr>')
-  k.set('n', '<space>tv', '<cmd>TestVisit<cr>')
+  local strategy = function()
+    if has_dap and dap.status() and dap.status():len() > 0 then
+      vim.notify('DAP is running, test will be run in background', 'info')
+      return 'dispatch_background'
+    else
+      return 'neovim'
+    end
+  end
+
+  local function run(command)
+    return function()
+      vim.cmd(command .. ' -strategy=' .. strategy())
+    end
+  end
+
+  k.set('n', '<space>tn', run('TestNearest'))
+  k.set('n', '<space>tf', run('TestFile'))
+  k.set('n', '<space>ts', run('TestSuite'))
+  k.set('n', '<space>tl', run('TestLast'))
+  k.set('n', '<space>tt', run('TestLast'))
+  k.set('n', '<space>tv', run('TestVisit'))
 end
 
 local function config()
@@ -27,12 +44,11 @@ local function config()
     end,
   })
 
-  vim.g['test#strategy'] = {
-    nearest = 'neovim',
-    file = 'neovim',
-    suite = 'neovim',
-  }
-  vim.g['test#elixir#exunit#options'] = '--trace'
+  vim.g['test#neovim#term_position'] = 'botright 20'
+  vim.g['test#strategy'] = 'neovim'
+  if cfg.get('test.phpunit.executable') then
+    vim.g['test#php#phpunit#executable'] = cfg.get('test.phpunit.executable')
+  end
 end
 
 return {
