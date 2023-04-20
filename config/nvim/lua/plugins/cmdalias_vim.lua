aed535cb32d60c65cea7132e4c56367c36b0902e
Pawel Bogut <pbogut@pbogut.me>
2023-04-20 19:15:48 +0200

    feat(nvim): update aliases

--- config/nvim/lua/plugins/cmdalias_vim.lua
+++ config/nvim/lua/plugins/cmdalias_vim.lua
@@ -3,10 +3,8 @@ local a = vim.cmd.Alias
 vim.api.nvim_create_autocmd('VimEnter', {
   group = vim.api.nvim_create_augroup('x_alias', { clear = true }),
   callback = function()
-    a('ag', 'Ag')
-    a('agg', 'Agg')
-    a('rg', 'Rg')
-    a('rgg', 'Rgg')
+    a('action', 'Action')
+    a('a', 'Action')
     a('art', 'Artisan')
     a('artisan', 'Artisan')
     a('git', 'Git')
@@ -18,6 +16,7 @@ vim.api.nvim_create_autocmd('VimEnter', {
     a('mix', 'Mix')
     a('repl', 'Repl')
     a('skel', 'Skel')
+    a('todo', 'Todo')
     a('db', 'DB')
     a('lg', 'LazyGit')
   end,
