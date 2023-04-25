5d586ce8f230fe8fe34265f105e83e74e695b1a7
Pawel Bogut <pbogut@pbogut.me>
2023-04-25 22:22:50 +0200

    feat: replace mutt ruby scripts with enrichmail

--- scripts/msmtp.sh
+++ scripts/msmtp.sh
@@ -1,27 +1,37 @@
 #!/usr/bin/env bash
 tmp_file=$(mktemp)
-cat /dev/stdin > $tmp_file
+cat /dev/stdin >"$tmp_file"
 set -e
 set -o pipefail
 
 from=$(exail -f "$tmp_file" --from-email)
 acc=${from##*@}
 
-# mutt-pre-process.rb -- adds html version of email using markdown
-# mutt-put-on-imap.rb -- puts copy of email on the imap server
-# mutt-add-tracking-pixel.rb -- adds tracking pixel
-# (because pixel is added after email is placed on imap server,
-#  you wont triger pixel event yourself)
-
 if [[ $1 == "--preview" ]]; then
-    preview="$(mktemp -d)/email.html"
-    cat $tmp_file | mutt-pre-process.rb --html-only > $preview
-    browser $preview > /dev/null 2>&1
-    exit 1
+  preview="$(mktemp -d)/email.html"
+  enrichmail --html-preview "$tmp_file" >"$preview"
+  browser "$preview" >/dev/null 2>&1
+  # perevent mutt form sending email
+  exit 1
 fi
 
-cat $tmp_file |
-    mutt-pre-process.rb |
-    mutt-put-on-imap.rb |
-    mutt-add-tracking-pixel.rb |
-    msmtp -a $acc --passwordeval="config email/$from/pass" --user=`config email/$from/user $from` --from=$from ${@:1}
+user=$(config "email/$from/user" "$from")
+pass=$(config "email/$from/pass")
+imap_url=$(config "email/$acc/imap/host")
+imap_port=$(config "email/$acc/imap/port" 993)
+tracking_base_url=$(config "email/open_tracking/api_url")
+
+# shellcheck disable=2068
+msmtp -a "$acc" \
+  --passwordeval="config email/$from/pass" \
+  --user="$user" \
+  --from="$from" ${@:1} \
+  <<<"$(enrichmail \
+    "$tmp_file" \
+    --generate-html \
+    --put-on-imap "INBOX" \
+    --add-pixel "$tracking_base_url" \
+    --user "$user" \
+    --password "$pass" \
+    --server "$imap_url" \
+    --port "$imap_port")"
