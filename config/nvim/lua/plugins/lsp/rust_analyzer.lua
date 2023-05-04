53ec47df105650aeec798cd920f5e12f8b1def8a
Pawel Bogut <pbogut@pbogut.me>
2023-05-04 19:09:10 +0200

    feat: wrap analyzer into separate script (rustup call was slowing vim start)

--- config/nvim/lua/plugins/lsp/rust_analyzer.lua
+++ config/nvim/lua/plugins/lsp/rust_analyzer.lua
@@ -2,9 +2,8 @@ local lspconfig = require('lspconfig')
 local me = {}
 
 function me.setup(opts)
-  local cmd = vim.fn.system('rustup which rust-analyzer'):gsub('\n', '')
   opts = vim.tbl_deep_extend('keep', opts, {
-    cmd = { cmd },
+    cmd = { 'rust-analyzer' },
   })
   lspconfig.rust_analyzer.setup(opts)
   lspconfig.rust_analyzer.setup = function(_) end
