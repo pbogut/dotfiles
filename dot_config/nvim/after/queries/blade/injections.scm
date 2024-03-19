ac220c1434fbe85e1042d783d70a86515052aa15
Pawel Bogut <pbogut@pbogut.me>
2024-03-19 09:10:21 +0100

    feat: update blade injections

--- dot_config/nvim/after/queries/blade/injections.scm
+++ dot_config/nvim/after/queries/blade/injections.scm
@@ -3,13 +3,8 @@
    (#set! injection.combined)
    (#set! injection.language php))
 
-;; ((php) @injection.content
-;;     (#set! injection.combined)
-;;     (#set! injection.language php))
-
 ((php_only) @injection.content
-    (#set! injection.combined)
-    (#set! injection.language php_only))
+   (#set! injection.language php_only))
 
 ((parameter) @injection.content
     (#set! injection.language php_only))
