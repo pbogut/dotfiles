787b007a58938ea62583ff24d9e948146f9c6428
Pawel Bogut <pbogut@pbogut.me>
2024-04-11 20:05:45 +0200

    feat: default outlook to inbox folder

---
+++ dot_config/private_Ferdium/recipes/office365-owa/user.js
@@ -0,0 +1,6 @@
+module.exports = (config, Ferdium) => {
+  if (window.location.host == 'www.microsoft.com') {
+    // window.location = 'https://outlook.office365.com/mail/';
+    window.location = 'https://outlook.office.com/mail/';
+  }
+};
