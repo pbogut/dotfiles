9a86f2b343ce370605b147d11c1876387a9d0d73
Pawel Bogut <pbogut@pbogut.me>
2024-06-16 21:28:22 +0200

    feat: f**k discord overlay

--- dot_scripts/executable_discord.sh
+++ dot_scripts/executable_discord.sh
@@ -4,7 +4,4 @@
 # author: Pawel Bogut <https://pbogut.me>
 # date:   19/03/2022
 #=================================================
-flatpak kill io.github.trigg.discover_overlay
-killall discord
-flatpak run io.github.trigg.discover_overlay &
 discord --enable-features=UseOzonePlatform --ozone-platform=wayland
