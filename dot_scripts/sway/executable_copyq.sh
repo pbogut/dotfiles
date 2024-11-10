c0db3226515e51f3ecd0627e297a497f7c8c3c83
Pawel Bogut <pbogut@pbogut.me>
2024-11-10 23:18:26 +0100

    feat: try replacing wofi with fuzzel

--- dot_scripts/sway/executable_copyq.sh
+++ dot_scripts/sway/executable_copyq.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 row=$(copyq 'dmenuList()' \
     | sed 's/!newline!/󰌑 /g' \
-    | wofi --dmenu -i -l 20 -p 'copyq:' --cache-file /dev/null \
+    | fuzzel --dmenu -p 'copyq: ' --cache /dev/null \
     | sed 's,\([0-9]*\).*,\1,')
 
 if [[ "$row" != "" ]]; then
