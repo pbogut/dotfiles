6acf48bdb08c3c105b85aee43c3880fd2929d4d9
Pawel Bogut <pbogut@pbogut.me>
2025-02-13 00:17:19 +0100

    feat: add zen user.js

---
+++ dot_zen/private_Personal/user.js
@@ -0,0 +1,3 @@
+user_pref("zen.welcome-screen.seen", true);
+user_pref("zen.tabs.vertical.right-side", true);
+user_pref("userChromeJS.enabled", true);
--- run_after_zen_work_profile_update.sh
+++ run_after_zen_work_profile_update.sh
@@ -6,6 +6,6 @@
 #=================================================
 mkdir -p "$HOME/.zen/Work/chrome"
 ln -sf "$HOME/.zen/Personal/zen-keyboard-shortcuts.json" "$HOME/.zen/Work/zen-keyboard-shortcuts.json"
+ln -sf "$HOME/.zen/Personal/user.js" "$HOME/.zen/Work/user.js"
 rm -fr "$HOME/.zen/Work/chrome"
 ln -sf "$HOME/.zen/Personal/chrome" "$HOME/.zen/Work/chrome"
-
