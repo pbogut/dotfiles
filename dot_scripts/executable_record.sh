e4b819ecf679badc1c27ab4a42c4ede9be9118e4
Pawel Bogut <pbogut@pbogut.me>
2025-05-15 11:07:02 +0200

    feat: add feadback to desktop recording

--- dot_scripts/executable_record.sh
+++ dot_scripts/executable_record.sh
@@ -6,4 +6,9 @@
 #=================================================
 #eval ffmpeg $(slop -f "-video_size %wx%h -framerate 25 -f x11grab -i $DISPLAY.0+%x+%y") $1
 
-wl-screenrec --geometry "$(slurp)" -f ~/Videos/"$(date +%Y-%m-%d_%H-%M-%S)".mp4
+filename="$HOME/Videos/$(date +%Y-%m-%d_%H-%M-%S).mp4"
+wl-screenrec --geometry "$(slurp)" -f "$filename"
+echo ""
+result=$(du -h "$filename" | awk '{print "Video saved: " $2 "\nTotal size: " $1 }')
+echo "$result"
+notify-send "$result" -i camera-video
