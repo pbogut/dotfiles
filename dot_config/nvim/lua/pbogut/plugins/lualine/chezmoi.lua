865cecefbbe74b136dc245f5dcce69e1a28e7418
Pawel Bogut <pbogut@pbogut.me>
2025-08-03 11:52:15 +0200

    fix: chezmoi icon got broken at some point

--- dot_config/nvim/lua/pbogut/plugins/lualine/chezmoi.lua
+++ dot_config/nvim/lua/pbogut/plugins/lualine/chezmoi.lua
@@ -1,12 +1,16 @@
 local colors = require('pbogut.settings.colors')
 vim.api.nvim_set_hl(0, 'lualine_chezmoi_icon', {
-  fg = colors.base02,
   bg = colors.green,
+  fg = colors.green,
+})
+vim.api.nvim_set_hl(0, 'lualine_chezmoi_actual_icon', {
+  fg = colors.green,
+  bg = colors.base02,
 })
 
 return function()
   if vim.b.chezmoi then
-    return '%#lualine_chezmoi_icon# %#lualine_c_normal#'
+    return '%#lualine_chezmoi_icon#%#lualine_chezmoi_actual_icon# %#lualine_c_normal#'
   else
     return ''
   end
