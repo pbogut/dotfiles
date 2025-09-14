2a4d4163a961cdbeb1a0988a8f5b0b4b96cfcebd
Pawel Bogut <pbogut@pbogut.me>
2025-09-14 20:31:01 +0200

    feat: use flatpack discord

--- dot_scripts/executable_discord.sh
+++ dot_scripts/executable_discord.sh
@@ -4,4 +4,5 @@
 # author: Pawel Bogut <https://pbogut.me>
 # date:   19/03/2022
 #=================================================
-discord --enable-features=UseOzonePlatform --ozone-platform=wayland
+# discord --enable-features=UseOzonePlatform --ozone-platform=wayland ||
+flatpak run --socket=wayland com.discordapp.Discord --enable-features=UseOzonePlatform --ozone-platform=wayland
