cef140822a16e3e760bfda2e95469042557b8fff
Pawel Bogut <pbogut@pbogut.me>
2025-09-30 21:39:47 +0200

    feat: replace mpvdlna with my go fork

---
+++ run_after_bild_mpvdlna.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#=================================================
+# name:   run_after_bild_mpvdlna
+# author: Pawel Bogut <pbogut@pbogut.me>
+# date:   30/09/2025
+#=================================================
+cd "$HOME/.config/mpv/scripts/mpvDLNA/goDLNA" && go build
