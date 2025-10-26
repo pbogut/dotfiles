9df409906c277d6068d0a5f7e0da1bc5b677dae9
Pawel Bogut <pbogut@pbogut.me>
2025-10-26 16:42:06 +0100

    feat: add screen recorder

--- dot_scripts/executable_record.sh
+++ dot_scripts/executable_record.sh
@@ -4,13 +4,29 @@
 # author: Pawel Bogut <http://pbogut.me>
 # date:   10/01/2018
 #=================================================
-#eval ffmpeg $(slop -f "-video_size %wx%h -framerate 25 -f x11grab -i $DISPLAY.0+%x+%y") $1
+refresh_waybar() {
+    for _ in {1..10}; do
+        sleep 0.5
+        pkill -RTMIN+21 -x waybar
+    done
+}
+
+if pgrep -f wf-record; then
+    pkill -INT -x wf-recorder;
+    refresh_waybar
+    exit 0
+fi
+
 mkdir -p "$HOME/Videos/screenrecords"
-filename="$HOME/Videos/screenrecords/screenrecord_from_$(date +%Y-%m-%d_%H-%M-%S).mp4"
 # wl-screenrec --geometry "$(slurp)" -f "$filename"
-wf-recorder -a -g "$(slurp)" -f "$filename"
-echo ""
-result=$(du -h "$filename" | awk '{print "Video saved: " $2 "\nTotal size: " $1 }')
-echo "$result"
-notify-send "Screen recording" "$result" -i camera-video
-lf-term "$filename"
+(
+    filename="$HOME/Videos/screenrecords/screenrecord_from_$(date +%Y-%m-%d_%H-%M-%S).mp4"
+    wf-recorder -a -g "$(slurp)" -f "$filename"
+    echo ""
+    result=$(du -h "$filename" | awk '{print "Video saved: " $2 "\nTotal size: " $1 }')
+    echo "$result"
+    notify-send "Screen recording" "$result" -i camera-video
+    lf-term "$filename"
+) &
+
+refresh_waybar
