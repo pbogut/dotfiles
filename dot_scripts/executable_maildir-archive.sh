52b68841386a7857c9c3a0663ae27df4edb52588
Pawel Bogut <pbogut@pbogut.me>
2025-11-04 22:52:28 +0100

    feat: change how maildir archive works

--- dot_scripts/executable_maildir-archive.sh
+++ dot_scripts/executable_maildir-archive.sh
@@ -4,8 +4,24 @@
 # author: Pawel Bogut <https://pbogut.me>
 # date:   11/12/2020
 #=================================================
+tmparchive="/storage/tmp/maildir-archive"
+mkdir -p "$tmparchive"
+rm -fr "${tmparchive:?}/"*
 
-find ~/Maildir -maxdepth 1 -type d -not -name '.*' -not -name 'Maildir' | while read fulldir; do
-  accountdir=$(basename $fulldir)
-  tar czvf /storage/nextcloud/Maildir.archive/$accountdir.tgz --exclude=\*.archive $fulldir
+find ~/Maildir -maxdepth 1 -type d -not -name '.*' -not -name 'Maildir' | while read -r fulldir; do
+  accountdir="$(basename "$fulldir")"
+  archivepath="$tmparchive/$accountdir-current.tgz"
+  tar czvf "$archivepath" --exclude=\*.archive "$fulldir"
 done
+
+find ~/Maildir -mindepth 1 -maxdepth 2 -type d -name '*.archive' | while read -r fulldir; do
+  accountdir="$(basename "$(dirname "$fulldir")")"
+  archivepath="$tmparchive/$accountdir-archive.tar"
+  if [[ -f "$archivepath" ]]; then
+    tar rvf "$archivepath" "$fulldir"
+  else
+    tar cvf "$archivepath" "$fulldir"
+  fi
+done
+
+mv "${tmparchive:?}"/* /storage/nextcloud/Maildir/
