c9f41def09ebe4586c11ac6adafa4e2d3928a0b7
Pawel Bogut <pbogut@pbogut.me>
2026-01-31 13:15:15 +0100

    feat: add gpt5.2 agent

--- dot_config/nvim/lua/pbogut/plugins/gp.lua
+++ dot_config/nvim/lua/pbogut/plugins/gp.lua
@@ -49,28 +49,40 @@ return {
           secret = vim.h.read_exec('bash ' .. os.getenv('HOME') .. '/.scripts/secret chatgpt/api_key'),
         },
       },
+      agents = {
+        {
+          provider = 'openai',
+          name = 'gpt-5.2',
+          chat = true,
+          command = false,
+          -- string with model name or table with model name and parameters
+          model = { model = 'gpt-5.2', temperature = 1.1, top_p = 1 },
+          -- system prompt (use this to specify the persona/role of the AI)
+          system_prompt = require('gp.defaults').chat_system_prompt,
+        },
+      },
     }
     require('gp').setup(conf)
 
     local command = vim.api.nvim_create_user_command
 
     command('GpChatFinder', function()
-      require("telescope.builtin").live_grep({
+      require('telescope.builtin').live_grep({
         attach_mappings = function(bufnr, map)
           map('i', '<C-n>', function()
-            require("telescope.actions").close(bufnr)
+            require('telescope.actions').close(bufnr)
             vim.cmd.GpChatNew()
           end)
           return true
         end,
-        prompt_title = "Search AI Chats (<C-n> to start new)",
-        cwd = require("gp").config.chat_dir,
-        default_text = "topic: ",
+        prompt_title = 'Search AI Chats (<C-n> to start new)',
+        cwd = require('gp').config.chat_dir,
+        default_text = 'topic: ',
         vimgrep_arguments = {
-          "rg",
-          "--column",
-          "--smart-case",
-          "--sortr=modified",
+          'rg',
+          '--column',
+          '--smart-case',
+          '--sortr=modified',
         },
       })
     end, {})
